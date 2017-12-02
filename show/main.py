6964f5de485ef37bf650b4311dfb649710c13d17
Rodny Molina <rmolina@linkedin.com>
2017-12-01 23:00:56 -0800

    First code-drop dealing with intf_description and intf_status enhancements
    I have taken care of the following items as part of this patch. There is another patch coming right after with changes made over a different repo:
    
     * Implemented a proper "show interface description" command -- i entirely removed the old one.
     * Enabled user to query system for a specific interface.
     * Extended "show interface status" command to do the same as above (query a specific interface).
     * Rewrote "show interface status" and integrated this code with "show interface description" one to prevent code-duplication and enhance modularity.
     * Added "speed" attribute as part of "show interface status" command.
    
    admin@lnos-x1-a-asw02:~$ show interfaces description
    Command: intfutil description
      Interface    Oper    Admin          Alias              Description
      -----------  ------  -------  -------------  -----------------------
      Ethernet112      up       up   fiftyGigE1/9  Special interface no. 1
      Ethernet114    down     down  fiftyGigE1/10                      N/A
      Ethernet116      up       up  fiftyGigE1/11  Special interface no. 2
      Ethernet118    down     down  fiftyGigE1/12                      N/A
      Ethernet120      up       up  fiftyGigE1/13                      N/A
      Ethernet122    down     down  fiftyGigE1/14                      N/A
      Ethernet124    down       up  fiftyGigE1/15                      N/A
    
    admin@lnos-x1-a-asw02:~$ show interfaces status | more
    Command: intfutil status
       Interface    Lanes    Speed    MTU          Alias    Oper    Admin
      -----------  -------  -------  -----  -------------  ------  -------
      Ethernet112  113,114      50G   9100   fiftyGigE1/9      up       up
      Ethernet114  115,116      N/A   1500  fiftyGigE1/10    down     down
      Ethernet116  117,118      50G   9100  fiftyGigE1/11      up       up
      Ethernet118  119,120      N/A   1500  fiftyGigE1/12    down     down
      Ethernet120  121,122      N/A   9100  fiftyGigE1/13      up       up
      Ethernet122  123,124      N/A   1500  fiftyGigE1/14    down     down
      Ethernet124  125,126      N/A   9100  fiftyGigE1/15    down       up

--- show/main.py
+++ show/main.py
@@ -205,6 +205,7 @@ def alias(interfacename):
 
     click.echo(tabulate(body, header))
 
+
 # 'summary' subcommand ("show interfaces summary")
 @interfaces.command()
 @click.argument('interfacename', required=False)
@@ -239,6 +240,7 @@ def basic(interfacename):
 
     run_command(command)
 
+
 @transceiver.command()
 @click.argument('interfacename', required=False)
 def details(interfacename):
@@ -251,15 +253,29 @@ def details(interfacename):
 
     run_command(command)
 
+
 @interfaces.command()
 @click.argument('interfacename', required=False)
 def description(interfacename):
     """Show interface status, protocol and description"""
 
     if interfacename is not None:
-        command = "sudo vtysh -c 'show interface {}'".format(interfacename)
+        command = "intfutil description {}".format(interfacename)
     else:
-        command = "sudo vtysh -c 'show interface description'"
+        command = "intfutil description"
+
+    run_command(command)
+
+
+@interfaces.command()
+@click.argument('interfacename', required=False)
+def status(interfacename):
+    """Show Interface status information"""
+
+    if interfacename is not None:
+        command = "intfutil status {}".format(interfacename)
+    else:
+        command = "intfutil status"
 
     run_command(command)
 
@@ -289,11 +305,6 @@ def counters(period, printall, clear):
 def portchannel():
     """Show PortChannel information"""
     run_command("teamshow")
-
-@interfaces.command()
-def status():
-    """Show Interface status information"""
-    run_command("interface_stat")
 
 
 #
