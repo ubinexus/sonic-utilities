5738d5170884be92d88c8a84f9350a913da41a87
Joe LeVeque <jolevequ@microsoft.com>
2018-01-12 20:16:33 +0000

    Increment package version to 1.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_test_suite():
 
 setup(
     name='sonic-utilities',
-    version='1.1',
+    version='1.2',
     description='Command-line utilities for SONiC',
     license='Apache 2.0',
     author='SONiC Team',
