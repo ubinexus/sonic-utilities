961059dccd57cec9b374f85b47f13cb572046da7
Pavel Shirshov <pavelsh@microsoft.com>
2018-07-09 17:27:49 -0700

    Add a stub for arp-helper-start'

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
     },
     scripts=[
         'scripts/aclshow',
+        'scripts/arp-helper-start',
         'scripts/boot_part',
         'scripts/coredump-compress',
         'scripts/decode-syseeprom',
