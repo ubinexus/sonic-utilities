69288dc63622da5d84a2464eb35184c31faefc89
Richard Wu <wutong23@baidu.com>
2019-07-02 15:25:18 +0800

    [acl_loader] show acl rule counter in command "show acl rule"              When showing acl rule, add the ACL rule counter in the              command output
    Signed-off-by: Richard Wu <wutong23@baidu.com>

--- acl_loader/main.py
+++ acl_loader/main.py
@@ -579,9 +579,12 @@ class AclLoader(object):
         :param rule_id: Optional. ACL rule name. Filter rule by specified rule name.
         :return:
         """
-        header = ("Table", "Rule", "Priority", "Action", "Match")
+        header = ("Table", "Rule", "Priority", "Action", "Match", "Packets", "Bytes")
 
         ignore_list = ["PRIORITY", "PACKET_ACTION", "MIRROR_ACTION"]
+
+        counter_db = SonicV2Connector(host="127.0.0.1")
+        counter_db.connect(counter_db.COUNTERS_DB)
 
         raw_data = []
         for (tname, rid), val in self.get_rules_db_info().iteritems():
@@ -609,7 +612,15 @@ class AclLoader(object):
             if len(matches) == 0:
                 matches.append("N/A")
 
-            rule_data = [[tname, rid, priority, action, matches[0]]]
+            count_packets = "N/A"
+            count_bytes = "N/A"
+            counters = counter_db.get_all(counter_db.COUNTERS_DB, "COUNTERS:{}:{}".format(tname, rid))
+            if isinstance(counters, dict):
+                if 'Packets' in counters:
+                    count_packets = counters['Packets']
+                if 'Bytes' in counters:
+                    count_bytes = counters['Bytes']
+            rule_data = [[tname, rid, priority, action, matches[0], count_packets, count_bytes]]
             if len(matches) > 1:
                 for m in matches[1:]:
                     rule_data.append(["", "", "", "", m])
