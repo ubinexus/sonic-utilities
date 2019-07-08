1f4904485e893a44ca8a530608015a62d4f86f98
Dante (Kuo-Jung) Su <dante.su@broadcom.com>
2019-07-08 13:47:29 +0800

    sonic_installer: remove '-C', it's not necessary
    Signed-off-by: Dante (Kuo-Jung) Su <dante.su@broadcom.com>

--- sonic_installer/main.py
+++ sonic_installer/main.py
@@ -327,7 +327,7 @@ def install(url, force):
             run_command("/usr/bin/unzip -od /tmp %s boot0" % image_path)
             run_command("swipath=%s target_path=/host sonic_upgrade=1 . /tmp/boot0" % image_path)
         else:
-            run_command("bash -C " + image_path)
+            run_command("bash " + image_path)
             run_command('grub-set-default --boot-directory=' + HOST_PATH + ' 0')
         run_command("rm -rf /host/old_config")
         # copy directories and preserve original file structure, attributes and associated metadata
