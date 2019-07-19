25fd75091bf7dc2aad1e3295a6c5975a7d1af86e
sschlafman <sschlafman@mellanox.com>
2019-07-19 15:20:12 -0700

    Fixed PR comments
    Fixed PR comments and added few improvments

--- mlnx-toolbox/mlnx-toolbox.py
+++ mlnx-toolbox/mlnx-toolbox.py
@@ -1,6 +1,6 @@
-#! /usr/bin/env python2
+#! /usr/bin/python
 """
-using sonic_sku_create.py  to create a new SONiC SKU based on a <SKU-DEF>.xml file
+using soni_sku_create.py  to create a new SONiC SKU based on a <SKU-DEF>.xml file
 
 usage: sonic_sku_create.py [-h] [-v] [-f FILE] [-b BASE] [-r]
                            [-c {new_sku_only,l2_mode_only,new_sku_l2}]
@@ -34,7 +34,6 @@ import subprocess
 import swsssdk
 import sys
 import shutil
-import sonic_platform
 
 from tabulate import tabulate
 import xml.etree.ElementTree as ET
@@ -45,57 +44,79 @@ DEFAULT_DEV_PATH = '/usr/share/sonic/device/'
 PORTCONFIG_HEADER = ["# name", "lanes", "speed", "alias", "index"]
 
 
-class SkuCreate(object):
+class MlnxToolbox(object):
 	"""
 	Process aclstat
 	"""
 
 	def __init__(self):
-		###self.metadata = {}
+		self.metadata = {}
+		self.mst_device = None
+		self.verbose = False
+		
 		self.portconfig_dict = {}
 		self.platform_specific_dict = {"x86_64-mlnx_msn2700-r0":self.msn2700_specific}
 		self.default_lanes_per_port = []
+		self.platform = 'NA'
 		self.fpp = []
 		self.fpp_split  =  {}
 		self.num_of_fpp = 0
 		self.num_of_lanes_dict = {"1" : 4, "2" : 2, "4" : 1}
 		
 		# Set up db connections
-		###self.db = swsssdk.SonicV2Connector(host="127.0.0.1")
-		###self.db.connect(self.db.CONFIG_DB)
-		
-		###self.configdb = swsssdk.ConfigDBConnector()
-		###self.configdb.connect()
+		self.db = swsssdk.SonicV2Connector(host="127.0.0.1")
+		self.cfgdb = swsssdk.ConfigDBConnector()
+		self.cfgdb.db_connect(self.db.APPL_DB)
+		self.db.connect(self.db.CONFIG_DB)
+		self.db.connect(self.db.APPL_DB)
 
-		###self.read_metadata()
-		try:
-			platform = subprocess.check_output("sonic-cfggen -H -v DEVICE_METADATA.localhost.platform",shell=True) #self.metadata['platform']
-			self.platform = platform.rstrip()
-		except KeyError:
-			print ("Couldn't find platform info in CONFIG_DB DEVICE_METADATA")
-		self.sku_name = None
-		self.base_sku_name = None
+		
+
+		self.read_metadata()
+		self.platform = self.metadata['platform']
+		self.hwsku = self.metadata['hwsku']
+		self.cfgmac = self.metadata['mac']
 		self.base_sku_dir = None
 		self.base_file_path = None		
 		self.new_sku_dir = None
-		self.version_info = sonic_platform.get_sonic_version_info()
-		self.build_version = format(self.version_info['build_version'])
 
 	def read_metadata(self):
 		"""
+		Redis database platform info from CONFIG_DB
+		"""
+		
+		self.metadata = self.db.get_all(self.db.CONFIG_DB, "DEVICE_METADATA|localhost" )
+		print (self.metadata)
+		
+	def get_interface_meta(self):
+		"""
 		Redis database platform info
 		"""
+		self.port_meta = self.db.get_all(self.db.APPL_DB,"PORT_TABLE:Ethernet0" )
+		print (self.port_meta)
+		self.port_meta1 = self.cfgdb.get_table(self.db.APPL_DB,"PORT_TABLE" )
+		print (self.port_meta1)
+				
+	def get_mst_device(self) :
+		p = subprocess.Popen("sudo mst status", shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+		out, err = p.communicate()
+		pattern = '(\/dev\/mst\/[0-9A-Za-z]{1,}_pci_cr0)'
 		try:
-			self.metadata = self.db.get_all(self.db.CONFIG_DB, "DEVICE_METADATA|localhost" )
-		except:
-			print("Error While trying to retrieve METADATA from CONFIG_DB")
-			
+			m = re.search(pattern,out)
+		except OSError as e:
+			print(e.message, file=sys.stderr) 
+		
+		self.mst_device = m.group(1)
+		if(self.verbose):
+			print (self.mst_device)
+		
+
+
 	def sku_def_parser(self,sku_def,sku_base) :
 		try:
 			f = open(sku_def,"r")
 		except IOError:
-			print ("Couldn't open file: " + sku_def)
-			exit()
+			print ("Couln't open file: " + sku_def)
 		element = ET.parse(f)
 		
 		root = element.getroot()
@@ -134,15 +155,13 @@ class SkuCreate(object):
 			f = open(self.base_file_path,"r")
 		except IOError:
 			print ("Could not open file "+ self.base_file_path)
-		line_header = f.next().split() # get the file header split into columns 
-		if line_header[0] == "#" : del line_header[0] # if hashtag is in a different column, remove it to align column header and data
-		alias_index = line_header.index('alias')
-		lanes_index = line_header.index('lanes')
+			
+		for i in range(1): f.next() # skip first  line
 		pattern = '^etp([0-9]{1,})'
 		for line in f:
 			line_arr = line.split()
-			m = re.match(pattern,line_arr[alias_index])
-			self.default_lanes_per_port.insert(int(m.group(1))-1,line_arr[lanes_index])
+			m = re.match(pattern,line_arr[2])
+			self.default_lanes_per_port.insert(int(m.group(1))-1,line_arr[1])
 		#print(self.default_lanes_per_port)
 		f.close()
 		
@@ -157,8 +176,7 @@ class SkuCreate(object):
 			idx_arr = sorted(values[1])
 
 			splt = len(splt_arr)
-			pattern = '(\d+),(\d+),(\d+),(\d+)'      #Currently the assumption is that the default(base) is 4 lanes
-
+			pattern = '(\d+),(\d+),(\d+),(\d+)'
 			m = re.match(pattern,self.default_lanes_per_port[int(fp)-1])
 			if (splt == 1) :
 				self.portconfig_dict[idx_arr[0]][lanes_index] = m.group(1)+","+m.group(2)+","+m.group(3)+","+m.group(4) 
@@ -221,9 +239,6 @@ class SkuCreate(object):
 		
 	def create_sku_dir(self) :
 		# create a new SKU directory based on the base SKU
-		if (os.path.exists(self.new_sku_dir)):
-			print("SKU directory: "+self.new_sku_dir+ " already exists\n Please use -r flag to remove the SKU dir first")
-			exit()
 		try:
 			shutil.copytree(self.base_sku_dir, self.new_sku_dir)
 		except OSError as e:
@@ -231,21 +246,11 @@ class SkuCreate(object):
 	
 	def remove_sku_dir(self) :
 		# remove SKU directory 
-		if (self.base_sku_dir == self.new_sku_dir) :
-			print ("Removing the base SKU" + self.new_sku_dir + " is not allowed")
-			exit()
 		try:
-			while True:
-				answer = raw_input("You are about to permanently delete the SKU "+ self.new_sku_dir+" !! \nDo you want to continue (Yes/No)?") 
-				if (answer == "Yes" or answer == "No"):
-					break
-				else:
-					print("Valid answers are Yes or No")
-			if (answer == "Yes") :
-				shutil.rmtree(self.new_sku_dir)
-				print ("SKU directory: "+ self.new_sku_dir + " was removed")
-			else :
-				print ("SKU directory: "+ self.new_sku_dir + " was NOT removed")
+			if (self.base_sku_dir == self.new_sku_dir) :
+				print ("Removing the base SKU" + self.new_sku_dir + " is not allowed")
+				return
+			shutil.rmtree(self.new_sku_dir)
 		except OSError as e:
 			print(e.message, file=sys.stderr) 
 					
@@ -273,8 +278,7 @@ class SkuCreate(object):
 				print ("MSN2700 -  even front panel ports (",fp,") are not allowed to split by 4")
 				return -1
 
-		
-	
+
 	def l2_mode(self,l2_sku) :
 		if not os.path.exists(DEFAULT_DEV_PATH+self.platform+"/"+l2_sku):
 			print ("Error - path:", l2_sku, " doesn't exist")
@@ -293,19 +297,24 @@ def main():
 	parser = argparse.ArgumentParser(description='Create a new SKU',
 									version='1.0.0',
 									formatter_class=argparse.RawTextHelpFormatter)
-	parser.add_argument('-f', '--file', action='store', help='SKU definition file. Must be provided when creating a new SKU', default=None)
+	parser.add_argument('-f', '--file', action='store', help='SKU definition file (default <cwd>/sku_def.xml)', default=None)
 	parser.add_argument('-b', '--base', action='store', help='SKU base definition  ', default=None)
 	parser.add_argument('-r', '--remove', action='store_true', help='Remove SKU folder')
 	parser.add_argument('-c', '--cmd', action='store', choices=['new_sku_only', 'l2_mode_only', 'new_sku_l2'], help='Choose action to preform (Generate a new SKU, Configure L2 mode, Both', default="new_sku_only")
-	parser.add_argument('-l2_sku_name', '--l2_sku_name', action='store', help='SKU name to be used in the L2 configuration mode', default=None)
+	parser.add_argument('-l2_sku_name', '--l2_sku_name', action='store', help='SKU name to be used in the L2 configuration mode (overrides sku name from file)', default=None)
 	parser.add_argument('-p', '--print', action='store_true', help='Print port_config.ini without creating a new SKU', default=False)
 	parser.add_argument('-vv', '--verbose', action='store_true', help='Verbose output', default=False)
 	args = parser.parse_args()
+	
 	l2_mode = False
 	sku_mode = False
 	sku_name = None
+	base = None
 	try:
-		sku = SkuCreate()
+		tb = MlnxToolbox()
+		tb.get_mst_device()
+		tb.get_interface_meta()
+		
 		if args.cmd == "l2_mode_only":
 			l2_mode = True
 			sku_mode = False
@@ -317,11 +326,10 @@ def main():
 			sku_mode = True		
 		
 		if args.file:
-			sku.sku_def_parser(args.file,args.base)
-		elif sku_mode:
-			print("SKU definition file was not provided (-f flag) while trying to create a new SKU.\n Only l2_mode_only command can omit the definition file")
-			exit() 
-			
+			SKU_DEF_FILE = args.file
+		else:
+			SKU_DEF_FILE = os.getcwd() + '/sku_def.xml'
+
 		if sku_mode :
 			if args.remove:
 				sku.remove_sku_dir()
