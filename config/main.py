3a952aed6a03752f4ea6023f78d42a6996610fd3
Olivier Singla <olivier.singla@broadcom.com>
2019-11-07 09:07:13 -0500

    In the event of a kernel crash, we need to gather as much information as possible to understand and identify the root cause of the crash. Currently, the kernel does not provide much information, which make kernel crash investigation difficult and time consuming.
    Fortunately, there is a way in the kernel to provide more information
    in the case of a kernel crash. kdump is a feature of the Linux kernel
    that creates crash dumps in the event of a kernel crash. This PR
    will add kernel kdump support.
    
    An extension to the CLI utilities config and show is provided to
     configure and manage kdump:
     - enable / disable kdump functionality
     - configure kdump
     - view kernel crash logs

--- config/main.py
+++ config/main.py
@@ -1233,6 +1233,55 @@ def shutdown():
     """Shut down BGP session(s)"""
     pass
 
+@config.group()
+def kdump():
+    """ Configure kdump """
+    if os.geteuid() != 0:
+        exit("Root privileges are required for this operation")
+    pass
+
+@kdump.command()
+def disable():
+    """Disable kdump operation!"""
+    config_db = ConfigDBConnector()
+    if config_db is not None:
+        config_db.connect()
+        config_db.mod_entry("KDUMP", "config", {"enabled": "false"})
+        run_command("sonic-kdump-config --disable")
+
+@kdump.command()
+@click.option('-y', '--yes', is_flag=True, help="Force rebooting after enabling kdump")
+def enable(yes):
+    """Enable kdump operation"""
+    config_db = ConfigDBConnector()
+    if config_db is not None:
+        config_db.connect()
+        config_db.mod_entry("KDUMP", "config", {"enabled": "true"})
+        if yes:
+            run_command("sonic-kdump-config --enable --yes")
+        else:
+            run_command("sonic-kdump-config --enable")
+
+@kdump.command()
+@click.argument('kdump_memory', metavar='<kdump_memory>', required=True)
+def memory(kdump_memory):
+    """Set memory allocated for kdump capture kernel"""
+    config_db = ConfigDBConnector()
+    if config_db is not None:
+        config_db.connect()
+        config_db.mod_entry("KDUMP", "config", {"memory": kdump_memory})
+        run_command("sonic-kdump-config --memory %s" % kdump_memory)
+
+@kdump.command()
+@click.argument('kdump_num_dumps', metavar='<kdump_num_dumps>', required=True, type=int)
+def num_dumps(kdump_num_dumps):
+    """Set max number of dump files for kdump"""
+    config_db = ConfigDBConnector()
+    if config_db is not None:
+        config_db.connect()
+        config_db.mod_entry("KDUMP", "config", {"num_dumps": kdump_num_dumps})
+        run_command("sonic-kdump-config --num_dumps %d" % kdump_num_dumps)
+
 # 'all' subcommand
 @shutdown.command()
 @click.option('-v', '--verbose', is_flag=True, help="Enable verbose output")
