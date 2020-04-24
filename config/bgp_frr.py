44043b92bca726e8d104307a6364626210a1d12a
Praveen Chaudhary <pchaudhary@linkedin.com>
2020-04-24 15:47:37 -0700

    [bgp_frr.py]: Resolving LGTM issues.

--- config/bgp_frr.py
+++ config/bgp_frr.py
@@ -1,66 +1,9 @@
 import click
 import re
-from config.main import *
 
 ##########################################################################
 # This file contains bgp group of config command for FRR Routing stack.  #
 ##########################################################################
-
-# Helper function 'config bgp ...' command
-def _frr_get_asn_from_ip_neighbour(ipaddr, verbose=False):
-    """
-    Get Local ASN for given ip address. Caller must verify
-    that ipaddr is our BGP neighbor.
-    :param ipaddr: ip address of neighbor
-    :return: Local ASN for BGP neighbor else Exception
-    """
-    vtyshCmd = "'show bgp neighbor {}'".format(ipaddr)
-    command = 'sudo vtysh -c {} | grep "local AS"'.format(vtyshCmd)
-    (out, err) = run_command(command, display_cmd=verbose, ignore_error=True, return_output=True)
-    if err:
-	print("Error: While searching for Local ASN of neighbor {}".format(ipaddr))
-	raise click.Abort
-
-    try:
-        localAsn = re.search(r"^.*local AS (.+?),.*$", out).group(1)
-        localAsnI = int(localAsn)
-        if verbose:
-            print("local ASN for neighbor {} is {}".format(ipaddr, localAsn))
-    except Exception as e:
-        print("Error: BGP information not found for Neighbor {}".format(ipaddr))
-        if verbose:
-            print("Exception:{}".format(e))
-        raise click.Abort
-
-    return localAsn
-
-def _frr_change_bgp_session_status(ipaddr, status, verbose):
-    """
-    Bring up/down BGP session with neighbor.
-    :param ipaddr: ip address of neighbor.
-    :param status: up or down.
-    :param verbose: Verbosity True or False.
-    :return: None
-    """
-    # Convert ip to lowercase to match with output of vtysh commands
-    ipaddr_lower = ipaddr.lower()
-
-    # Get Local ASN for Neighbour
-    asn = _frr_get_asn_from_ip_neighbour(ipaddr_lower, verbose)
-
-    if status == 'down':
-        vtyshCmd = "'configure terminal' -c 'router bgp {}'".format(asn)
-        vtyshCmd = vtyshCmd + " -c 'neighbor {} shutdown'".format(ipaddr_lower)
-        command = "sudo vtysh -c {}".format(vtyshCmd)
-        run_command(command=command, display_cmd=verbose, ignore_error=False)
-    elif status == 'up':
-        vtyshCmd = "'configure terminal' -c 'router bgp {}'".format(asn)
-        vtyshCmd = vtyshCmd + " -c 'no neighbor {} shutdown'".format(ipaddr_lower)
-        command = "sudo vtysh -c {}".format(vtyshCmd)
-        run_command(command=command, display_cmd=verbose, ignore_error=False)
-
-    return
-
 #
 # 'bgp' group
 #
@@ -112,3 +55,67 @@ def all(verbose):
 def neighbor(ipaddr, verbose):
     """Start up BGP session by neighbor IP address"""
     _frr_change_bgp_session_status(ipaddr, 'up', verbose)
+
+
+# Helper function 'config bgp ...' command
+def _frr_get_asn_from_ip_neighbour(ipaddr, verbose=False):
+    """
+    Get Local ASN for given ip address. Caller must verify
+    that ipaddr is our BGP neighbor.
+    :param ipaddr: ip address of neighbor
+    :return: Local ASN for BGP neighbor else Exception
+    """
+    vtyshCmd = "'show bgp neighbor {}'".format(ipaddr)
+    command = 'sudo vtysh -c {} | grep "local AS"'.format(vtyshCmd)
+    (out, err) = run_command(command, display_cmd=verbose, ignore_error=True, return_output=True)
+    if err:
+	print("Error: While searching for Local ASN of neighbor {}".format(ipaddr))
+	raise click.Abort
+
+    try:
+        localAsn = re.search(r"^.*local AS (.+?),.*$", out).group(1)
+        localAsnI = int(localAsn)
+        if verbose:
+            print("local ASN for neighbor {} is {}".format(ipaddr, localAsnI))
+    except Exception as e:
+        print("Error: BGP information not found for Neighbor {}".format(ipaddr))
+        if verbose:
+            print("Exception:{}".format(e))
+        raise click.Abort
+
+    return localAsn
+
+"""
+Keeping import config.main below to avoid cyclic dependency
+"""
+from config.main import run_command
+
+"""
+Help change bgp session for FRR
+"""
+def _frr_change_bgp_session_status(ipaddr, status, verbose):
+    """
+    Bring up/down BGP session with neighbor.
+    :param ipaddr: ip address of neighbor.
+    :param status: up or down.
+    :param verbose: Verbosity True or False.
+    :return: None
+    """
+    # Convert ip to lowercase to match with output of vtysh commands
+    ipaddr_lower = ipaddr.lower()
+
+    # Get Local ASN for Neighbour
+    asn = _frr_get_asn_from_ip_neighbour(ipaddr_lower, verbose)
+
+    if status == 'down':
+        vtyshCmd = "'configure terminal' -c 'router bgp {}'".format(asn)
+        vtyshCmd = vtyshCmd + " -c 'neighbor {} shutdown'".format(ipaddr_lower)
+        command = "sudo vtysh -c {}".format(vtyshCmd)
+        run_command(command=command, display_cmd=verbose, ignore_error=False)
+    elif status == 'up':
+        vtyshCmd = "'configure terminal' -c 'router bgp {}'".format(asn)
+        vtyshCmd = vtyshCmd + " -c 'no neighbor {} shutdown'".format(ipaddr_lower)
+        command = "sudo vtysh -c {}".format(vtyshCmd)
+        run_command(command=command, display_cmd=verbose, ignore_error=False)
+
+    return
