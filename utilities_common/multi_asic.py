4c1b7880eec2a18a641a0cc7ada76637c2621426
Arvindsrinivasan Lakshmi Narasimhan <arlakshm@microsoft.com>
2020-07-30 11:50:24 -0700

    Move some functions to new package
    Signed-off-by: Arvindsrinivasan Lakshmi Narasimhan <arlakshm@microsoft.com>

--- utilities_common/multi_asic.py
+++ utilities_common/multi_asic.py
@@ -5,15 +5,14 @@ import subprocess
 
 import click
 
-from sonic_py_common.device_info import get_all_namespaces
-from sonic_py_common.device_info import get_namespaces
-from sonic_py_common.device_info import is_multi_asic
-from sonic_py_common.device_info import is_bgp_session_internal
-from sonic_py_common.device_info import is_port_channel_internal
-from sonic_py_common.device_info import is_port_internal
-from swsssdk import ConfigDBConnector
-from swsssdk import SonicDBConfig
-from swsssdk import SonicV2Connector
+from sonic_py_common.multi_asic_device_info import connect_config_db_for_ns
+from sonic_py_common.multi_asic_device_info import connect_to_all_dbs_for_ns
+from sonic_py_common.multi_asic_device_info import get_all_namespaces
+from sonic_py_common.multi_asic_device_info import get_namespace_list
+from sonic_py_common.multi_asic_device_info import is_multi_asic
+from sonic_py_common.multi_asic_device_info import is_bgp_session_internal
+from sonic_py_common.multi_asic_device_info import is_port_channel_internal
+from sonic_py_common.multi_asic_device_info import is_port_internal
 from utilities_common.constants import DEFAULT_NAMESPACE
 from utilities_common.constants import DISPLAY_ALL
 from utilities_common.constants import DISPLAY_EXTERNAL
@@ -27,40 +26,8 @@ class MultiAsic(object):
     def __init__(self, display_option=DISPLAY_ALL, namespace_option=None):
         self.namespace_option = namespace_option
         self.display_option = display_option
-        SonicDBConfig.load_sonic_global_db_config()
         self.current_namespace = None
         self.is_multi_asic = is_multi_asic()
-
-    def connect_dbs_for_ns(self,namespace=DEFAULT_NAMESPACE):
-        '''
-        The function connects to the DBs for a given namespace and
-        returns the handle
-        If no namespace is provided, it will connect to the db in the
-        default namespace.
-        In case of multi ASIC, the default namespace is the database instance running the on the host
-        In case of single ASIC, the namespace has to be DEFAULT_NAMESPACE
-        '''
-
-        db = SonicV2Connector(use_unix_socket_path=True, namespace=namespace)
-        db.connect(db.APPL_DB)
-        db.connect(db.CONFIG_DB)
-        db.connect(db.STATE_DB)
-        db.connect(db.COUNTERS_DB)
-        db.connect(db.ASIC_DB)
-        return db
-
-    def connect_config_db_for_ns(self, namespace=DEFAULT_NAMESPACE):
-        '''
-        The function connects to the config DB for a given namespace and
-        returns the handle
-        If no namespace is provided, it will connect to the db in the
-        default namespace.
-        In case of multi ASIC, the default namespace is the database instance running the on the host
-        In case of single ASIC, the namespace has to be DEFAULT_NAMESPACE
-        '''
-        config_db = ConfigDBConnector(use_unix_socket_path=True, namespace=namespace)
-        config_db.connect()
-        return config_db
 
     def is_object_internal(self, object_type, cli_object):
         '''
@@ -97,21 +64,25 @@ class MultiAsic(object):
         else:
             namespaces = get_all_namespaces()
             if self.namespace_option is None:
-                if self.display_option ==  DISPLAY_ALL:
+                if self.display_option == DISPLAY_ALL:
                     ns_list = namespaces['front_ns'] + namespaces['back_ns']
                 else:
                     ns_list = namespaces['front_ns']
             else:
-                if self.namespace_option not in get_namespaces():
-                    raise ValueError('Unknown Namespace {}'.format(self.namespace_option))
+                if self.namespace_option not in namespaces['front_ns'] and \
+                        self.namespace_option not in namespaces['back_ns']:
+                    raise ValueError(
+                        'Unknown Namespace {}'.format(self.namespace_option))
                 ns_list = [self.namespace_option]
         return ns_list
-    
+
+
 def multi_asic_ns_choices():
-    if not is_multi_asic() :
+    if not is_multi_asic():
         return [DEFAULT_NAMESPACE]
-    choices =  get_namespaces()
+    choices = get_namespace_list()
     return choices
+
 
 def multi_asic_display_choices():
     if not is_multi_asic():
@@ -119,22 +90,35 @@ def multi_asic_display_choices():
     else:
         return [DISPLAY_ALL, DISPLAY_EXTERNAL]
 
+
 def multi_asic_display_default_option():
     if not is_multi_asic():
         return DISPLAY_ALL
     else:
-        return  DISPLAY_EXTERNAL
+        return DISPLAY_EXTERNAL
+
 
 _multi_asic_click_options = [
-    click.option('--display', '-d', 'display', default=multi_asic_display_default_option(), show_default=True, type=click.Choice(multi_asic_display_choices()), help='Show internal interfaces'),
-    click.option('--namespace', '-n', 'namespace', default=None, type=click.Choice(multi_asic_ns_choices()), show_default=True, help='Namespace name or all'),
-    #click.option('--namespace', '-n', 'namespace',  callback=multi_asic_ns_callback, show_default=True, help='Namespace name or all'),
-    ]
+    click.option('--display',
+                 '-d', 'display',
+                 default=multi_asic_display_default_option(),
+                 show_default=True,
+                 type=click.Choice(multi_asic_display_choices()),
+                 help='Show internal interfaces'),
+    click.option('--namespace',
+                 '-n', 'namespace',
+                 default=None,
+                 type=click.Choice(multi_asic_ns_choices()),
+                 show_default=True,
+                 help='Namespace name or all'),
+]
+
 
 def multi_asic_click_options(func):
     for option in reversed(_multi_asic_click_options):
         func = option(func)
     return func
+
 
 def run_on_all_asics(func):
     '''
@@ -147,18 +131,22 @@ def run_on_all_asics(func):
     '''
     @functools.wraps(func)
     def wrapped_run_on_all_asics(self, *args, **kwargs):
-        ns_list =  self.multi_asic.get_ns_list_based_on_options()
+        ns_list = self.multi_asic.get_ns_list_based_on_options()
         for ns in ns_list:
             self.multi_asic.current_namespace = ns
-            self.db = self.multi_asic.connect_dbs_for_ns(ns)
-            self.config_db = self.multi_asic.connect_config_db_for_ns(ns)
+            self.db = connect_to_all_dbs_for_ns(ns)
+            self.config_db = connect_config_db_for_ns(ns)
             func(self,  *args, **kwargs)
     return wrapped_run_on_all_asics
 
+
 def multi_asic_args(parser=None):
     if parser is None:
-        parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+        parser = argparse.ArgumentParser(
+            formatter_class=argparse.RawTextHelpFormatter)
 
-    parser.add_argument('-d','--display',   default=DISPLAY_EXTERNAL, help='Display all interfaces or only external interfaces')
-    parser.add_argument('-n','--namespace',   default=None, help='Display interfaces for specific namespace')
+    parser.add_argument('-d', '--display', default=DISPLAY_EXTERNAL,
+                        help='Display all interfaces or only external interfaces')
+    parser.add_argument('-n', '--namespace', default=None,
+                        help='Display interfaces for specific namespace')
     return parser
