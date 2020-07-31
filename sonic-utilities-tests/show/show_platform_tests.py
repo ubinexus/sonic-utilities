1e7c2e2dd17c24a9282e4a770eaf2702b52a449d
Joe LeVeque <jolevequ@microsoft.com>
2020-07-31 23:52:26 +0000

    [tests] Add unit tests for 'show platform ...' commands

---
+++ sonic-utilities-tests/show/show_platform_tests.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import textwrap
+
+from click.testing import CliRunner
+from unittest import TestCase, mock
+
+test_path = os.path.dirname(os.path.abspath(__file__))
+modules_path = os.path.dirname(test_path)
+sys.path.insert(0, test_path)
+sys.path.insert(0, modules_path)
+
+import show.main as show
+
+
+TEST_PLATFORM = "x86_64-mlnx_msn2700-r0"
+TEST_HWSKU = "Mellanox-SN2700"
+TEST_ASIC_TYPE = "mellanox"
+
+
+class TestShowPlatform(TestCase):
+    @classmethod
+    def setup_class(cls):
+        print("SETUP")
+        os.environ["UTILITIES_UNIT_TESTING"] = "1"
+
+    def setUp(self):
+        self.runner = CliRunner()
+
+    # Test 'show platform summary'
+    def test_summary(self):
+        expected_output = """\
+            Platform: {}
+            HwSKU: {}
+            ASIC: {}
+            """.format(TEST_PLATFORM, TEST_HWSKU, TEST_ASIC_TYPE)
+
+        with mock.patch("show.get_hw_info_dict",
+                        return_value={"platform": TEST_PLATFORM, "hwsku": TEST_HWSKU, "asic_type": TEST_ASIC_TYPE}):
+            result = self.runner.invoke(show.cli.commands["platform"].commands["summary"], [])
+            assert result.output == textwrap.dedent(expected_output)
+
+    @classmethod
+    def teardown_class(cls):
+        print("TEARDOWN")
+        os.environ["PATH"] = os.pathsep.join(os.environ["PATH"].split(os.pathsep)[:-1])
+        os.environ["UTILITIES_UNIT_TESTING"] = "0"
