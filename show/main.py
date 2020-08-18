78d9128b7e2c2db46d29f1c8434817b5a4a58542
kktheballer <kavinkamaraj@gmail.com>
2020-08-17 18:43:56 -0700

    Adding show-nhg-group command
    --> It is identical to show-nhg-hash-view
    w/ the addition of hashbucketsize as well as
    the nhg prefix

--- show/main.py
+++ show/main.py
@@ -1944,6 +1944,82 @@ def fg_nhg_hash_view(nhg):
     header = ["Next-Hop-Key", "Hash Bucket #"]
     click.echo(tabulate(table, header))
 
+@cli.command('fg-nhg-group')
+@click.argument('nhg', required=True)
+def fg_nhg_group(nhg):
+   
+    state_db = SonicV2Connector(host='127.0.0.1')
+    try:
+    	state_db.connect(state_db.STATE_DB, False)   # Make one attempt only STATE_DB
+    except:
+	click.echo("STATE_DB Table does not exist!")
+	exit()
+	   
+    TABLE_NAME_SEPARATOR = '|'
+    prefix = 'FG_ROUTE_TABLE' + TABLE_NAME_SEPARATOR
+    _hash = '{}{}'.format(prefix, '*')
+    table_keys = []  
+
+    try:  
+	table_keys = state_db.keys(state_db.STATE_DB, _hash)
+    except:
+        click.echo("FG_ROUTE_TABLE does not exist!")
+	exit()
+
+    if table_keys is None:	
+	click.echo("FG_ROUTE_TABLE does not contain any keys!")
+	exit()   
+
+    t_dict = {}
+    table = []
+    ip_prefix = ""
+    key_avail = {"ipv4":None, "ipv6": None}
+
+    for tk in table_keys:
+	if "." in tk:
+		key_avail['ipv4'] = tk
+	elif ":" in tk:
+		key_avail['ipv6'] = tk
+
+    if nhg == "fgnhg_v4":
+	if key_avail["ipv4"] is not None:
+		t_dict = state_db.get_all(state_db.STATE_DB, key_avail["ipv4"])
+		ip_prefix = key_avail["ipv4"]
+	else:
+		click.echo("IPV4 nhg does not exist!")
+		exit()
+
+    if nhg == "fgnhg_v6":
+	if key_avail["ipv6"] is not None:
+		t_dict = state_db.get_all(state_db.STATE_DB, key_avail["ipv6"])
+		ip_prefix = key_avail["ipv6"]
+	else:
+		click.echo("IPV6 nhg does not exist!")
+		exit()
+
+
+    hash_bucket_size = len(t_dict)		    
+	     
+    vals = sorted(set([val for val in t_dict.values()]))
+	 
+    for nh_ip in vals:
+	bank_ids = [int(k) for k,v in t_dict.items() if v == nh_ip]
+	r = []  
+        r.append(nh_ip.split("@")[0])
+	r.append(sorted(bank_ids))
+	table.append(r)
+    
+    	
+	
+    header = ["Next-Hop-Key", "Hash Bucket #"]
+    click.echo ("\nNHG_PREFIX: ")
+    click.echo (ip_prefix.split("|")[1])
+    click.echo ("\nHash Bucket Size: ")
+    click.echo (hash_bucket_size)
+    click.echo ("\n")
+    click.echo(tabulate(table, header))
+
+
 
 #
 # 'reboot-cause' command ("show reboot-cause")
