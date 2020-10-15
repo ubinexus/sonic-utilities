51c0b96c174fe2559a297416c15ca01ac3302ee1
Tamer Ahmed <tamer.ahmed@microsoft.com>
2020-10-14 17:18:13 -0700

    [counterpoll] Disable Counter Poll When Entering Fast Reboot
    Counter polling is enabled via enable_counters script when booting
    into new image and these configuration is written to config_db.
    However, subsequent fast-reboot into the same image will have counter
    polling enabled by default. This affect fast-reboot time. As a workaround,
    we will disable counter poll when entering fast-reboot.
    
    singed-off-by: Tamer Ahmed <tamer.ahmed@microsoft.com>

--- tests/counterpoll_test.py
+++ tests/counterpoll_test.py
@@ -1,10 +1,12 @@
-import sys
-import os
-import time
-import pytest
 import click
+import json
+import os
+import pytest
 import swsssdk
+import sys
+import time
 from click.testing import CliRunner
+from shutil import copyfile
 
 test_path = os.path.dirname(os.path.abspath(__file__))
 modules_path = os.path.dirname(test_path)
@@ -50,6 +52,38 @@ class TestCounterpoll(object):
         expected = "Invalid value for \"POLL_INTERVAL\": 1000 is not in the valid range of 30000 to 300000."
         assert result.exit_code == 2
         assert expected in result.output
+
+    @pytest.fixture(scope='class')
+    def _get_config_db_file(self):
+        sample_config_db_file = os.path.join(test_path, "counterpoll_input", "config_db.json")
+        config_db_file = os.path.join('/', "tmp", "config_db.json")
+        copyfile(sample_config_db_file, config_db_file)
+
+        yield config_db_file
+
+        os.remove(config_db_file)
+
+    @pytest.mark.parametrize("status", ["disable", "enable"])
+    def test_update_counter_config_db_status(self, status, _get_config_db_file):
+        runner = CliRunner()
+        result = runner.invoke(counterpoll.cli.commands["config-db"].commands[status], [_get_config_db_file])
+
+        with open(_get_config_db_file) as json_file:
+            config_db = json.load(json_file)
+        counters = [
+            "QUEUE_WATERMARK",
+            "BUFFER_POOL_WATERMARK",
+            "PFCWD",
+            "QUEUE",
+            "PG_WATERMARK",
+            "PORT_BUFFER_DROP",
+            "RIF",
+            "PORT",
+        ]
+        for counter in counters:
+            if counter in config_db["FLEX_COUNTER_TABLE"] and \
+                "FLEX_COUNTER_STATUS" in config_db["FLEX_COUNTER_TABLE"][counter]:
+                assert config_db["FLEX_COUNTER_TABLE"][counter]["FLEX_COUNTER_STATUS"] == status
 
     @classmethod
     def teardown_class(cls):
