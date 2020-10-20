fbf7b2289677da78c6399d40dd0e04d2cd5f2aa2
dgsudharsan <sudharsan_gopalarat@dell.com>
2020-10-19 19:52:36 -0700

    Addressing code review comments

--- scripts/db_migrator.py
+++ scripts/db_migrator.py
@@ -211,15 +211,15 @@ class DBMigrator():
             if self.mellanox_buffer_migrator.mlnx_migrate_buffer_pool_size('version_1_0_3', 'version_1_0_4') and self.mellanox_buffer_migrator.mlnx_migrate_buffer_profile('version_1_0_3', 'version_1_0_4'):
                 self.set_version('version_1_0_4')
 
-        self.migrate_copp_table()
         self.set_version('version_1_0_4')
         return 'version_1_0_4'
 
     def version_1_0_4(self):
         """
-        Current latest version. Nothing to do here.
+        Current latest version. Always perform copp table migration at current latest version
         """
         log.log_info('Handling version_1_0_4')
+        self.migrate_copp_table()
         return None
 
     def get_version(self):
