5651b1719c92d0f53b11a21a3fa024d243472e19
sujinmkang <sujkang@microsoft.com>
2020-10-22 23:46:02 -0700

    fix unit test

--- tests/show_reboot_cause_test.py
+++ tests/show_reboot_cause_test.py
@@ -43,7 +43,7 @@ name                 cause        time                          user    comment
 2020_10_09_02_33_06  reboot       Fri Oct  9 02:29:44 UTC 2020  admin
 """
         runner = CliRunner()
-        result = runner.invoke(show.cli.commands["reboot-cause"], ["history"])
+        result = runner.invoke(show.cli.commands["reboot-cause"].commands["history"], [])
         assert result.output == expected_output
 
     @classmethod
