74fcd7235cafbb47a3ad2fd0b4da69bd37df4cf7
Blueve <672454911@qq.com>
2020-11-04 07:24:00 +0000

    [connect][clear] Support --devicename option for connect/clear line commands

--- consutil/main.py
+++ consutil/main.py
@@ -52,7 +52,7 @@ def show(db, brief):
 @consutil.command()
 @clicommon.pass_db
 @click.argument('target')
-@click.option('--devicename', '-d', is_flag=True, help="clear by name - if flag is set, interpret linenum as device name instead")
+@click.option('--devicename', '-d', is_flag=True, help="clear by name - if flag is set, interpret target as device name instead")
 def clear(db, target, devicename):
     """Clear preexisting connection to line"""
     # identify the target line
@@ -70,7 +70,7 @@ def clear(db, target, devicename):
 @consutil.command()
 @clicommon.pass_db
 @click.argument('target')
-@click.option('--devicename', '-d', is_flag=True, help="connect by name - if flag is set, interpret linenum as device name instead")
+@click.option('--devicename', '-d', is_flag=True, help="connect by name - if flag is set, interpret target as device name instead")
 def connect(db, target, devicename):
     """Connect to switch via console device - TARGET is line number or device name of switch"""
     # identify the target line
