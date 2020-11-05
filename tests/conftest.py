2d087669273d835041959839fe828c9cf3c59c23
Joe LeVeque <jolevequ@microsoft.com>
2020-11-05 01:44:08 +0000

    Fix conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ import pytest
 from sonic_py_common import device_info
 from swsssdk import ConfigDBConnector
 
-from . import mock_tables.dbconnector
+from .mock_tables import dbconnector
 
 
 test_path = os.path.dirname(os.path.abspath(__file__))
