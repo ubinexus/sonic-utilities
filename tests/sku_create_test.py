9417e85263d84655179965b26805d351dbf3072c
Joe LeVeque <jolevequ@microsoft.com>
2020-11-12 19:44:21 +0000

    Fix the path of the sonic_sku_create.py script

--- tests/sku_create_test.py
+++ tests/sku_create_test.py
@@ -12,7 +12,7 @@ scripts_path = os.path.join(modules_path, "scripts")
 input_path = os.path.join(modules_path, "tests/sku_create_input")
 output_dir_path = os.path.join(modules_path, "tests/sku_create_input/Mellanox-SN2700-D48C8_NEW")
 sku_def_file = os.path.join(input_path, "Mellanox-SN2700-D48C8.xml")
-sku_create_script = os.path.join(scripts_path, "sonic_sku_create.py")
+sku_create_script = "sonic_sku_create.py"
 output_file_path = os.path.join(modules_path, "tests/sku_create_input/Mellanox-SN2700-D48C8_NEW/port_config.ini")
 model_file_path = os.path.join(modules_path, "tests/sku_create_input/Mellanox-SN2700-D48C8/port_config.ini")
 
