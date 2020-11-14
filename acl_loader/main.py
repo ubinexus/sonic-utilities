bec1ae865d896fc26c81be9dc87e53fa309851b7
Joe LeVeque <jolevequ@microsoft.com>
2020-11-14 21:56:33 +0000

    [acl_loader/main.py] Update methods from ipaddr module forms to ipaddress forms

--- acl_loader/main.py
+++ acl_loader/main.py
@@ -430,14 +430,14 @@ class AclLoader(object):
 
         if rule.ip.config.source_ip_address:
             source_ip_address = rule.ip.config.source_ip_address.encode("ascii")
-            if ipaddress.IPNetwork(source_ip_address).version == 4:
+            if ipaddress.ip_network(source_ip_address).version == 4:
                 rule_props["SRC_IP"] = source_ip_address
             else:
                 rule_props["SRC_IPV6"] = source_ip_address
 
         if rule.ip.config.destination_ip_address:
             destination_ip_address = rule.ip.config.destination_ip_address.encode("ascii")
-            if ipaddress.IPNetwork(destination_ip_address).version == 4:
+            if ipaddress.ip_network(destination_ip_address).version == 4:
                 rule_props["DST_IP"] = destination_ip_address
             else:
                 rule_props["DST_IPV6"] = destination_ip_address
