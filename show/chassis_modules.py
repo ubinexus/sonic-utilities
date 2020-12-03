e3b92309bb98ef3e64b2926cfa6ff6c264eed3ec
mprabhu <manjunath.prabhu@nokia.com>
2020-12-02 23:51:18 -0800

    Moving SonicV2Connector to swsscommon

--- show/chassis_modules.py
+++ show/chassis_modules.py
@@ -1,7 +1,7 @@
 import click
 from natsort import natsorted
 from tabulate import tabulate
-from swsssdk import SonicV2Connector
+from swsscommon.swsscommon import SonicV2Connector
 
 import utilities_common.cli as clicommon
 
