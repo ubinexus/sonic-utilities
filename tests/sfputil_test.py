dbce4c3f48cf2bc247107b82d43e46e82739196b
Joe LeVeque <jolevequ@microsoft.com>
2021-02-11 21:37:31 +0000

    Add unit test for format_dict_value_to_string()

--- tests/sfputil_test.py
+++ tests/sfputil_test.py
@@ -18,3 +18,53 @@ class TestSfputil(object):
         runner = CliRunner()
         result = runner.invoke(sfputil.cli.commands["version"], [])
         assert result.output.rstrip() == "sfputil version {}".format(sfputil.VERSION)
+
+    def test_format_dict_value_to_string(self):
+        sorted_key_table = [
+            'rx1power',
+            'rx2power',
+            'rx3power',
+            'rx4power',
+            'tx1bias',
+            'tx1power',
+            'tx2bias',
+            'tx2power',
+            'tx3bias',
+            'tx3power',
+            'tx4bias',
+            'tx4power'
+        ]
+
+        dom_info_dict = {
+            'temperature': '41.7539C',
+            'voltage': '3.2577Volts',
+            'rx1power': '-1.6622dBm',
+            'rx2power': '-1.7901dBm',
+            'rx3power': '-1.6973dBm',
+            'rx4power': '-2.0915dBm',
+            'tx1bias': '35.8400mA',
+            'tx2bias': '37.5780mA',
+            'tx3bias': '35.8400mA',
+            'tx4bias': '35.8400mA',
+            'tx1power': 'N/A',
+            'tx2power': 'N/A',
+            'tx3power': 'N/A',
+            'tx4power': 'N/A'
+        }
+
+        expected_output = '''\
+                RX1Power: -1.6622dBm
+                RX2Power: -1.7901dBm
+                RX3Power: -1.6973dBm
+                RX4Power: -2.0915dBm
+                TX1Bias: 35.8400mA
+                TX2Bias: 37.5780mA
+                TX3Bias: 35.8400mA
+                TX4Bias: 35.8400mA
+'''
+
+        output = sfputil.format_dict_value_to_string(sorted_key_table,
+                                                     dom_info_dict,
+                                                     sfputil.QSFP_DOM_CHANNEL_MONITOR_MAP,
+                                                     sfputil.DOM_VALUE_UNIT_MAP)
+        assert output == expected_output
