cfee0b23fd643c4f57cfb745c334d4a9ed1d8619
Joe LeVeque <jolevequ@microsoft.com>
2021-02-11 00:46:52 +0000

    Add unit test for 'sfputil version'

---
+++ tests/sfputil_test.py
@@ -0,0 +1,25 @@
+import sys
+import os
+from unittest import mock
+
+import pytest
+from click.testing import CliRunner
+
+test_path = os.path.dirname(os.path.abspath(__file__))
+modules_path = os.path.dirname(test_path)
+sys.path.insert(0, modules_path)
+
+
+@pytest.fixture(scope="class")
+def do_imports():
+    with mock.patch.dict('sys.modules', sonic_platform=mock.MagicMock()):
+        import sfputil.main as sfputil
+        yield
+
+
+@pytest.mark.usefixtures("do_imports")
+class TestSfputil(object):
+    def test_version(self):
+        runner = CliRunner()
+        result = runner.invoke(sfputil.cli.commands["version"], [])
+        assert result.output == "sfputil version {}".format(sfputil.VERSION)
