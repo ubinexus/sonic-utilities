29c22a4c979ba9cfe17d34ef3600a6119ca774c5
Arun Barboza <29963827+a-barboza@users.noreply.github.com>
2021-03-23 14:52:02 -0700

    RADIUS Management User Authentication Feature
    HLD: https://github.com/Azure/SONiC/blob/master/doc/aaa/radius_authentication.md
    
    CLI:
    
    admin@sonic:~$ show radius
    RADIUS global auth_type pap (default)
    RADIUS global retransmit 3 (default)
    RADIUS global timeout 5 (default)
    RADIUS global passkey <EMPTY_STRING> (default)
    
    admin@sonic:~$
    
    admin@sonic:~$ sudo config radius
    Usage: config radius [OPTIONS] COMMAND [ARGS]...
    
      RADIUS server configuration
    
    Options:
      -?, -h, --help  Show this message and exit.
    
    Commands:
      add         Specify a RADIUS server
      authtype    Specify RADIUS server global auth_type [chap | pap | mschapv2]
      default     set its default configuration
      delete      Delete a RADIUS server
      nasip       Specify RADIUS server global NAS-IP|IPV6-Address <IPAddress>
      passkey     Specify RADIUS server global passkey <STRING>
      retransmit  Specify RADIUS server global retry attempts <0 - 10>
      sourceip    Specify RADIUS server global source ip <IPAddress>
      statistics  Specify RADIUS server global statistics [enable | disable |...
      timeout     Specify RADIUS server global timeout <1 - 60>
    admin@sonic:~$

--- show/main.py
+++ show/main.py
@@ -1281,6 +1281,55 @@ def tacacs():
                 output += ('               %s %s\n' % (key, str(entry[key])))
     click.echo(output)
 
+@cli.command()
+def radius():
+    """Show RADIUS configuration"""
+    config_db = ConfigDBConnector()
+    config_db.connect()
+    output = ''
+    data = config_db.get_table('RADIUS')
+
+    radius = {
+        'global': {
+            'auth_type': 'pap (default)',
+            'retransmit': '3 (default)',
+            'timeout': '5 (default)',
+            'passkey': '<EMPTY_STRING> (default)'
+        }
+    }
+    if 'global' in data:
+        radius['global'].update(data['global'])
+    for key in radius['global']:
+        output += ('RADIUS global %s %s\n' % (str(key), str(radius['global'][key])))
+
+    data = config_db.get_table('RADIUS_SERVER')
+    if data != {}:
+        for row in data:
+            entry = data[row]
+            output += ('\nRADIUS_SERVER address %s\n' % row)
+            for key in entry:
+                output += ('               %s %s\n' % (key, str(entry[key])))
+
+    counters_db = SonicV2Connector(host='127.0.0.1')
+    counters_db.connect(counters_db.COUNTERS_DB, retry_on=False)
+
+    if radius['global'].get('statistics', False) and (data != {}):
+        for row in data:
+            exists = counters_db.exists(counters_db.COUNTERS_DB,
+                                     'RADIUS_SERVER_STATS:{}'.format(row))
+            if not exists:
+                continue
+
+            counter_entry = counters_db.get_all(counters_db.COUNTERS_DB,
+                    'RADIUS_SERVER_STATS:{}'.format(row))
+            output += ('\nStatistics for RADIUS_SERVER address %s\n' % row)
+            for key in counter_entry:
+                if counter_entry[key] != "0":
+                    output += ('               %s %s\n' % (key, str(counter_entry[key])))
+    counters_db.close(counters_db.COUNTERS_DB)
+
+    click.echo(output)
+
 #
 # 'mirror_session' command  ("show mirror_session ...")
 #
