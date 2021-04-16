ecd651968357dc562373c4df0c48e19a476595c9
ghooo <ghooo.ghooo@gmail.com>
2021-04-15 17:59:50 -0700

    fix generic_config_updater path in test file

--- tests/generic_config_updater/generic_updater_test.py
+++ tests/generic_config_updater/generic_updater_test.py
@@ -6,7 +6,13 @@ import sys
 import unittest
 from imp import load_source
 from unittest.mock import Mock, call
-sys.path.insert(1, '../../generic_config_updater')
+
+generic_config_updater_test_path = os.path.dirname(os.path.abspath(__file__))
+test_path = os.path.dirname(generic_config_updater_test_path)
+modules_path = os.path.dirname(test_path)
+generic_config_updater_path = os.path.join(modules_path, "generic_config_updater")
+sys.path.insert(0, generic_config_updater_path)
+
 import generic_updater as gu
 
 class MockSideEffectDict:
