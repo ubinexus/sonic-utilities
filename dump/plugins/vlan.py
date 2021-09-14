788b326da168cc480948b05241b9d2977a4146fa
Raphael Tryster <raphaelt@nvidia.com>
2021-09-14 17:38:39 +0000

    [Debug Dump Util] Vlan module additional tables and tests requested by Sud
    Signed-off-by: Raphael Tryster <raphaelt@nvidia.com>

--- dump/plugins/vlan.py
+++ dump/plugins/vlan.py
@@ -27,8 +27,8 @@ class Vlan(Executor):
         self.init_vlan_member_appl_info(vlan_name)
         self.init_state_vlan_info(vlan_name)
         self.init_state_vlan_member_info(vlan_name)
-        self.init_asic_vlan_info(vlan_name)
-        self.init_asic_vlan_member_info(vlan_name)
+        req, vlan_table = self.init_asic_vlan_info(vlan_name)
+        self.init_asic_vlan_member_info(vlan_name, req, vlan_table)
         return self.ret_temp
     
     def init_vlan_config_info(self, vlan_name):
@@ -86,7 +86,7 @@ class Vlan(Executor):
         # Convert 'Vlanxxx' to 'xxx'
         if vlan_name[0:4] != "Vlan" or not vlan_name[4:].isnumeric():
             self.ret_temp["ASIC_DB"]["tables_not_found"] =["ASIC_STATE:SAI_OBJECT_TYPE_VLAN"]
-            return
+            return {}, {}
         vlan_num = int(vlan_name[4:])
         
         # Find the table named "ASIC_STATE:SAI_OBJECT_TYPE_VLAN:*" in which SAI_VLAN_ATTR_VLAN_ID = vlan_num
@@ -97,27 +97,61 @@ class Vlan(Executor):
             self.ret_temp[req.db]["keys"] = ret["keys"]
         else:
             self.ret_temp[req.db]["tables_not_found"] = [req.table]
+            
+        # Return request and table to caller in case its next call is to init_asic_vlan_member_info which also needs it
+        return req, ret
     
-    def init_asic_vlan_member_info(self, vlan_name):
-        # Convert 'Vlanxxx' to 'xxx'
+    def init_asic_vlan_member_info(self, vlan_name, req, vlan_table):
+        
+        bridge_oids = []
+        ret = {}
+        
+        # Handle invalid vlan name 
         if vlan_name[0:4] != "Vlan" or not vlan_name[4:].isnumeric():
             self.ret_temp["ASIC_DB"]["tables_not_found"].append("ASIC_STATE:SAI_OBJECT_TYPE_VLAN_MEMBER")
+            self.ret_temp["ASIC_DB"]["tables_not_found"].append("ASIC_STATE:SAI_OBJECT_TYPE_BRIDGE_PORT")
             return
-        vlan_num = int(vlan_name[4:])
         
-        # Find the table named "ASIC_STATE:SAI_OBJECT_TYPE_VLAN:*" in which SAI_VLAN_ATTR_VLAN_ID = vlan_num
-        req = MatchRequest(db="ASIC_DB", table="ASIC_STATE:SAI_OBJECT_TYPE_VLAN", key_pattern="*", 
-                           field="SAI_VLAN_ATTR_VLAN_ID", value=str(vlan_num), ns=self.ns)
-        ret = self.match_engine.fetch(req)
+        # Find vlan table if not passed by caller
+        if not req or not vlan_table:
+        
+            # Convert 'Vlanxxx' to 'xxx'
+            vlan_num = int(vlan_name[4:])
+    
+            # Find the table named "ASIC_STATE:SAI_OBJECT_TYPE_VLAN:*" in which SAI_VLAN_ATTR_VLAN_ID = vlan_num
+            req = MatchRequest(db="ASIC_DB", table="ASIC_STATE:SAI_OBJECT_TYPE_VLAN", key_pattern="*", 
+                               field="SAI_VLAN_ATTR_VLAN_ID", value=str(vlan_num), ns=self.ns)
+            vlan_table = self.match_engine.fetch(req)
 
         # Find all the member tables whose vlan is the oid:0x... part of the table name just found 
-        if not ret["error"] and len(ret["keys"]) != 0:
+        if not vlan_table["error"] and len(vlan_table["keys"]) != 0:
             req = MatchRequest(db="ASIC_DB", table="ASIC_STATE:SAI_OBJECT_TYPE_VLAN_MEMBER", key_pattern="*", 
-                               field="SAI_VLAN_MEMBER_ATTR_VLAN_ID", value=ret["keys"][0][32:], ns=self.ns)
+                               field="SAI_VLAN_MEMBER_ATTR_VLAN_ID", value=vlan_table["keys"][0][32:], 
+                               return_fields=["SAI_VLAN_MEMBER_ATTR_BRIDGE_PORT_ID"], ns=self.ns)
             ret = self.match_engine.fetch(req)
-        if not ret["error"] and len(ret["keys"]) != 0:
+
+        # Retrieve SAI_OBJECT_TYPE_VLAN_MEMBER and SAI_OBJECT_TYPE_BRIDGE_PORT tables for each member port 
+        if ret and not ret["error"] and len(ret["keys"]) != 0:
             for mem in ret["keys"]:
+
+                # Append ASIC_STATE:SAI_OBJECT_TYPE_VLAN_MEMBER:oid:... directly to output
                 self.ret_temp[req.db]["keys"].append(mem)
+
+                # Find the SAI_OBJECT_TYPE_BRIDGE_PORT key corresponding to member's SAI_VLAN_MEMBER_ATTR_BRIDGE_PORT_ID
+                req = MatchRequest(db="ASIC_DB", table="ASIC_STATE:SAI_OBJECT_TYPE_BRIDGE_PORT", 
+                                   key_pattern=ret['return_values'][mem]['SAI_VLAN_MEMBER_ATTR_BRIDGE_PORT_ID'], ns=self.ns)
+                bridge_ret = self.match_engine.fetch(req)
+                
+                # Append ASIC_STATE:SAI_OBJECT_TYPE_BRIDGE_PORT:oid:... to a separate list
+                if bridge_ret and not bridge_ret["error"] and len(bridge_ret["keys"]) != 0:
+                    bridge_oids.append(bridge_ret['keys'][0])
+                else:
+                    self.ret_temp[req.db]["tables_not_found"].append("ASIC_STATE:SAI_OBJECT_TYPE_BRIDGE_PORT")
+                
+            # Append the collection of ASIC_STATE:SAI_OBJECT_TYPE_BRIDGE_PORT:oid:... to the output after the vlan members
+            for bp in bridge_oids:
+                self.ret_temp[req.db]["keys"].append(bp)
         else:
             self.ret_temp[req.db]["tables_not_found"].append("ASIC_STATE:SAI_OBJECT_TYPE_VLAN_MEMBER")
+            self.ret_temp[req.db]["tables_not_found"].append("ASIC_STATE:SAI_OBJECT_TYPE_BRIDGE_PORT")
         
--- tests/dump_tests/module_tests/vlan_test.py
+++ tests/dump_tests/module_tests/vlan_test.py
@@ -85,35 +85,15 @@ class TestVlanModule:
         expect["STATE_DB"]["keys"].append("VLAN_TABLE|Vlan4")
         expect["STATE_DB"]["keys"].append("VLAN_MEMBER_TABLE|Vlan4|Ethernet16")
         expect["STATE_DB"]["keys"].append("VLAN_MEMBER_TABLE|Vlan4|Ethernet24")
-        expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_VLAN:oid:0x260000000005e5")
-        expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_VLAN_MEMBER:oid:0x270000000005e7")
-        expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_VLAN_MEMBER:oid:0x270000000005e9")
+        expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_VLAN:oid:0x2600000000062b")
+        expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_VLAN_MEMBER:oid:0x27000000000631")
+        expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_VLAN_MEMBER:oid:0x27000000000633")
+        expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_BRIDGE_PORT:oid:0x3a000000000630")
+        expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_BRIDGE_PORT:oid:0x3a000000000632")
         ddiff = DeepDiff(sort_lists(returned), sort_lists(expect), ignore_order=True)
         assert not ddiff, ddiff
         
-    def test_missing_interface(self, match_engine):
-        params = {}
-        params[Vlan.ARG_NAME] = "Vlan3"
-        params["namespace"] = ""
-        m_vlan = Vlan(match_engine)
-        returned = m_vlan.execute(params)
-        expect = create_template_dict(dbs=["CONFIG_DB", "APPL_DB", "ASIC_DB", "STATE_DB"])
-        expect["CONFIG_DB"]["keys"].append("VLAN|Vlan3")
-        expect["CONFIG_DB"]["keys"].append("VLAN_MEMBER|Vlan3|Ethernet0")
-        expect["CONFIG_DB"]["keys"].append("VLAN_MEMBER|Vlan3|Ethernet8")
-        expect["APPL_DB"]["keys"].append("VLAN_TABLE:Vlan3")
-        expect["APPL_DB"]["keys"].append("VLAN_MEMBER_TABLE:Vlan3:Ethernet0")
-        expect["APPL_DB"]["keys"].append("VLAN_MEMBER_TABLE:Vlan3:Ethernet8")
-        expect["STATE_DB"]["keys"].append("VLAN_TABLE|Vlan3")
-        expect["STATE_DB"]["keys"].append("VLAN_MEMBER_TABLE|Vlan3|Ethernet0")
-        expect["STATE_DB"]["keys"].append("VLAN_MEMBER_TABLE|Vlan3|Ethernet8")
-        expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_VLAN:oid:0x260000000005e0")
-        expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_VLAN_MEMBER:oid:0x270000000005e4")
-        expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_VLAN_MEMBER:oid:0x270000000005e2")
-        ddiff = DeepDiff(sort_lists(returned), sort_lists(expect), ignore_order=True)
-        assert not ddiff, ddiff
-    
-    def test_missing_memebers_and_interface(self, match_engine):
+    def test_missing_members(self, match_engine):
         params = {}
         params[Vlan.ARG_NAME] = "Vlan2"
         params["namespace"] = ""
@@ -126,8 +106,9 @@ class TestVlanModule:
         expect["APPL_DB"]["tables_not_found"].append("VLAN_MEMBER_TABLE")
         expect["STATE_DB"]["keys"].append("VLAN_TABLE|Vlan2")
         expect["STATE_DB"]["tables_not_found"].append("VLAN_MEMBER_TABLE")
-        expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_VLAN:oid:0x260000000005df")
+        expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_VLAN:oid:0x26000000000629")
         expect["ASIC_DB"]["tables_not_found"].append("ASIC_STATE:SAI_OBJECT_TYPE_VLAN_MEMBER")
+        expect["ASIC_DB"]["tables_not_found"].append("ASIC_STATE:SAI_OBJECT_TYPE_BRIDGE_PORT")
         ddiff = DeepDiff(returned, expect, ignore_order=True)
         assert not ddiff, ddiff
     
@@ -146,6 +127,7 @@ class TestVlanModule:
         expect["STATE_DB"]["tables_not_found"].append("VLAN_MEMBER_TABLE")
         expect["ASIC_DB"]["tables_not_found"].append("ASIC_STATE:SAI_OBJECT_TYPE_VLAN")
         expect["ASIC_DB"]["tables_not_found"].append("ASIC_STATE:SAI_OBJECT_TYPE_VLAN_MEMBER")
+        expect["ASIC_DB"]["tables_not_found"].append("ASIC_STATE:SAI_OBJECT_TYPE_BRIDGE_PORT")
         ddiff = DeepDiff(returned, expect, ignore_order=True)
         assert not ddiff, ddiff
     
@@ -164,10 +146,11 @@ class TestVlanModule:
         expect["STATE_DB"]["tables_not_found"].append("VLAN_MEMBER_TABLE")
         expect["ASIC_DB"]["tables_not_found"].append("ASIC_STATE:SAI_OBJECT_TYPE_VLAN")
         expect["ASIC_DB"]["tables_not_found"].append("ASIC_STATE:SAI_OBJECT_TYPE_VLAN_MEMBER")
+        expect["ASIC_DB"]["tables_not_found"].append("ASIC_STATE:SAI_OBJECT_TYPE_BRIDGE_PORT")
         ddiff = DeepDiff(returned, expect, ignore_order=True)
         assert not ddiff, ddiff
     
-    def test_garbage_vlan(self, match_engine):
+    def test_garbage_alpha_vlan(self, match_engine):
         params = {}
         params[Vlan.ARG_NAME] = "garbage"
         params["namespace"] = ""
@@ -182,13 +165,104 @@ class TestVlanModule:
         expect["STATE_DB"]["tables_not_found"].append("VLAN_MEMBER_TABLE")
         expect["ASIC_DB"]["tables_not_found"].append("ASIC_STATE:SAI_OBJECT_TYPE_VLAN")
         expect["ASIC_DB"]["tables_not_found"].append("ASIC_STATE:SAI_OBJECT_TYPE_VLAN_MEMBER")
+        expect["ASIC_DB"]["tables_not_found"].append("ASIC_STATE:SAI_OBJECT_TYPE_BRIDGE_PORT")
         ddiff = DeepDiff(returned, expect, ignore_order=True)
         assert not ddiff, ddiff
     
+    def test_garbage_number_vlan(self, match_engine):
+        params = {}
+        params[Vlan.ARG_NAME] = "614"
+        params["namespace"] = ""
+        m_vlan = Vlan(match_engine)
+        returned = m_vlan.execute(params)
+        expect = create_template_dict(dbs=["CONFIG_DB", "APPL_DB", "ASIC_DB", "STATE_DB"])
+        expect["CONFIG_DB"]["tables_not_found"].append("VLAN")
+        expect["CONFIG_DB"]["tables_not_found"].append("VLAN_MEMBER")
+        expect["APPL_DB"]["tables_not_found"].append("VLAN_TABLE")
+        expect["APPL_DB"]["tables_not_found"].append("VLAN_MEMBER_TABLE")
+        expect["STATE_DB"]["tables_not_found"].append("VLAN_TABLE")
+        expect["STATE_DB"]["tables_not_found"].append("VLAN_MEMBER_TABLE")
+        expect["ASIC_DB"]["tables_not_found"].append("ASIC_STATE:SAI_OBJECT_TYPE_VLAN")
+        expect["ASIC_DB"]["tables_not_found"].append("ASIC_STATE:SAI_OBJECT_TYPE_VLAN_MEMBER")
+        expect["ASIC_DB"]["tables_not_found"].append("ASIC_STATE:SAI_OBJECT_TYPE_BRIDGE_PORT")
+        ddiff = DeepDiff(returned, expect, ignore_order=True)
+        assert not ddiff, ddiff
+    
+    # Vlan6 is not defined in appl_db
+    def test_missing_appl_db(self, match_engine):
+        params = {}
+        params[Vlan.ARG_NAME] = "Vlan6"
+        params["namespace"] = ""
+        m_vlan = Vlan(match_engine)
+        returned = m_vlan.execute(params)
+        expect = create_template_dict(dbs=["CONFIG_DB", "APPL_DB", "ASIC_DB", "STATE_DB"])
+        expect["CONFIG_DB"]["keys"].append("VLAN|Vlan6")
+        expect["CONFIG_DB"]["keys"].append("VLAN_MEMBER|Vlan6|Ethernet32")
+        expect["CONFIG_DB"]["keys"].append("VLAN_MEMBER|Vlan6|Ethernet40")
+        expect["APPL_DB"]["tables_not_found"].append("VLAN_TABLE")
+        expect["APPL_DB"]["tables_not_found"].append("VLAN_MEMBER_TABLE")
+        expect["STATE_DB"]["keys"].append("VLAN_TABLE|Vlan6")
+        expect["STATE_DB"]["keys"].append("VLAN_MEMBER_TABLE|Vlan6|Ethernet32")
+        expect["STATE_DB"]["keys"].append("VLAN_MEMBER_TABLE|Vlan6|Ethernet40")
+        expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_VLAN:oid:0x2600000000061c")
+        expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_VLAN_MEMBER:oid:0x2700000000061e")
+        expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_VLAN_MEMBER:oid:0x2700000000061f")
+        expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_BRIDGE_PORT:oid:0x3a00000000061d")
+        expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_BRIDGE_PORT:oid:0x3a000000000618")
+        ddiff = DeepDiff(sort_lists(returned), sort_lists(expect), ignore_order=True)
+        assert not ddiff, ddiff
+        
+    # Vlan7 is not defined in state_db, but adds bridge port tables in asic_db
+    def test_missing_state_db(self, match_engine):
+        params = {}
+        params[Vlan.ARG_NAME] = "Vlan7"
+        params["namespace"] = ""
+        m_vlan = Vlan(match_engine)
+        returned = m_vlan.execute(params)
+        expect = create_template_dict(dbs=["CONFIG_DB", "APPL_DB", "ASIC_DB", "STATE_DB"])
+        expect["CONFIG_DB"]["keys"].append("VLAN|Vlan7")
+        expect["CONFIG_DB"]["keys"].append("VLAN_MEMBER|Vlan7|Ethernet48")
+        expect["CONFIG_DB"]["keys"].append("VLAN_MEMBER|Vlan7|Ethernet56")
+        expect["APPL_DB"]["keys"].append("VLAN_TABLE:Vlan7")
+        expect["APPL_DB"]["keys"].append("VLAN_MEMBER_TABLE:Vlan7:Ethernet48")
+        expect["APPL_DB"]["keys"].append("VLAN_MEMBER_TABLE:Vlan7:Ethernet56")
+        expect["STATE_DB"]["tables_not_found"].append("VLAN_TABLE")
+        expect["STATE_DB"]["tables_not_found"].append("VLAN_MEMBER_TABLE")
+        expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_VLAN:oid:0x26000000000620")
+        expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_VLAN_MEMBER:oid:0x27000000000623")
+        expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_VLAN_MEMBER:oid:0x27000000000621")
+        expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_BRIDGE_PORT:oid:0x3a000000000622")
+        expect["ASIC_DB"]["keys"].append("ASIC_STATE:SAI_OBJECT_TYPE_BRIDGE_PORT:oid:0x3a00000000061a")
+        ddiff = DeepDiff(sort_lists(returned), sort_lists(expect), ignore_order=True)
+        assert not ddiff, ddiff
+        
+    # Vlan8 is not defined in asic_db
+    def test_missing_asic_db(self, match_engine):
+        params = {}
+        params[Vlan.ARG_NAME] = "Vlan8"
+        params["namespace"] = ""
+        m_vlan = Vlan(match_engine)
+        returned = m_vlan.execute(params)
+        expect = create_template_dict(dbs=["CONFIG_DB", "APPL_DB", "ASIC_DB", "STATE_DB"])
+        expect["CONFIG_DB"]["keys"].append("VLAN|Vlan8")
+        expect["CONFIG_DB"]["keys"].append("VLAN_MEMBER|Vlan8|Ethernet64")
+        expect["CONFIG_DB"]["keys"].append("VLAN_MEMBER|Vlan8|Ethernet72")
+        expect["APPL_DB"]["keys"].append("VLAN_TABLE:Vlan8")
+        expect["APPL_DB"]["keys"].append("VLAN_MEMBER_TABLE:Vlan8:Ethernet64")
+        expect["APPL_DB"]["keys"].append("VLAN_MEMBER_TABLE:Vlan8:Ethernet72")
+        expect["STATE_DB"]["keys"].append("VLAN_TABLE|Vlan8")
+        expect["STATE_DB"]["keys"].append("VLAN_MEMBER_TABLE|Vlan8|Ethernet64")
+        expect["STATE_DB"]["keys"].append("VLAN_MEMBER_TABLE|Vlan8|Ethernet72")
+        expect["ASIC_DB"]["tables_not_found"].append("ASIC_STATE:SAI_OBJECT_TYPE_VLAN")
+        expect["ASIC_DB"]["tables_not_found"].append("ASIC_STATE:SAI_OBJECT_TYPE_VLAN_MEMBER")
+        expect["ASIC_DB"]["tables_not_found"].append("ASIC_STATE:SAI_OBJECT_TYPE_BRIDGE_PORT")
+        ddiff = DeepDiff(sort_lists(returned), sort_lists(expect), ignore_order=True)
+        assert not ddiff, ddiff
+        
     def test_all_args(self, match_engine):
         params = {}
         m_vlan = Vlan(match_engine)
         returned = m_vlan.get_all_args("")
-        expect = ["Vlan2", "Vlan3", "Vlan4"]
+        expect = ["Vlan2", "Vlan3", "Vlan4", "Vlan6", "Vlan7", "Vlan8"]
         ddiff = DeepDiff(expect, returned, ignore_order=True)
         assert not ddiff, ddiff 
