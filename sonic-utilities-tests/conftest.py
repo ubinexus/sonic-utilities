30720dfe7e91ca40b1b17f42a5eae8766303c0de
bingwang <wang.bing@microsoft.com>
2021-09-30 12:33:59 +0000

    Fix UT
    Signed-off-by: bingwang <wang.bing@microsoft.com>

--- sonic-utilities-tests/conftest.py
+++ sonic-utilities-tests/conftest.py
@@ -4,6 +4,7 @@ import sys
 
 import mock
 import pytest
+from sonic_py_common import device_info
 
 # noinspection PyUnresolvedReferences
 import mock_tables.dbconnector
@@ -21,6 +22,63 @@ import utilities_common.constants as constants
 test_path = os.path.dirname(os.path.abspath(__file__))
 modules_path = os.path.dirname(test_path)
 sys.path.insert(0, modules_path)
+
+generated_services_list = [
+        'ntp-config.service',
+        'warmboot-finalizer.service',
+        'watchdog-control.service',
+        'rsyslog-config.service',
+        'interfaces-config.service',
+        'hostcfgd.service',
+        'hostname-config.service',
+        'topology.service',
+        'updategraph.service',
+        'config-setup.service',
+        'caclmgrd.service',
+        'procdockerstatsd.service',
+        'pcie-check.service',
+        'process-reboot-cause.service',
+        'dhcp_relay.service',
+        'snmp.service',
+        'sflow.service',
+        'bgp.service',
+        'telemetry.service',
+        'swss.service',
+        'database.service',
+        'database.service',
+        'lldp.service',
+        'lldp.service',
+        'pmon.service',
+        'radv.service',
+        'mgmt-framework.service',
+        'nat.service',
+        'teamd.service',
+        'syncd.service',
+        'snmp.timer',
+        'telemetry.timer']
+
+@pytest.fixture
+def get_cmd_module():
+    import config.main as config
+    import show.main as show
+
+    return (config, show)
+
+def set_mock_apis():
+    import config.main as config
+    cwd = os.path.dirname(os.path.realpath(__file__))
+    config.asic_type = mock.MagicMock(return_value="broadcom")
+    config._get_device_type = mock.MagicMock(return_value="ToRRouter")
+
+@pytest.fixture
+def setup_single_broadcom_asic():
+    import config.main as config
+    import show.main as show
+
+    set_mock_apis()
+    device_info.get_num_npus = mock.MagicMock(return_value=1)
+    config._get_sonic_generated_services = \
+        mock.MagicMock(return_value=(generated_services_list, []))
 
 @pytest.fixture
 def setup_single_bgp_instance(request):
