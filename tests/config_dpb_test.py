23920032b4abd32457e1381082f21f03ac7f20ad
sabarivel sakthivel <sabarivel.sakthivel@broadcom.com>
2021-10-12 01:37:36 -0700

    add mclag config for dbp test

--- tests/config_dpb_test.py
+++ tests/config_dpb_test.py
@@ -860,7 +860,20 @@ configDbJson =  {
             "speed": "25000",
             "admin_status": "up"
         }
-    }
+    },
+    "VLAN_INTERFACE": {
+        "Vlan100": {},
+        "Vlan100|100.0.0.1/20": {}
+    },
+    "MCLAG_DOMAIN": {
+		"1": {
+			"source_ip": "100.0.0.1",
+			"keepalive_interval": "1",
+			"session_timeout": "30",
+			"peer_ip": "100.0.0.2",
+			"peer_link": "Ethernet0"
+			}
+	}  
 }
 
 portBreakOutConfigDbJson = {
