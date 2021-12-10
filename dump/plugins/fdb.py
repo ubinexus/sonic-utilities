8f45ab8ae232933d60304ce608985a088a5641ba
Sudharsan Dhamal Gopalarathnam <sudharsand@nvidia.com>
2021-12-10 00:47:44 +0000

    Correcting typo

--- dump/plugins/fdb.py
+++ dump/plugins/fdb.py
@@ -35,13 +35,13 @@ class Fdb(Executor):
     def init_fdb_appl_info(self, fdb_name):
         req = MatchRequest(db="APPL_DB", table="FDB_TABLE", key_pattern=fdb_name, ns=self.ns)
         ret = self.match_engine.fetch(req)
-        self.add_to_ret_template(req.table, req.db, ret["keys"], ret["error"], false)
+        self.add_to_ret_template(req.table, req.db, ret["keys"], ret["error"], False)
         req = MatchRequest(db="APPL_DB", table="VXLAN_FDB_TABLE", key_pattern=fdb_name, ns=self.ns)
         ret = self.match_engine.fetch(req)
-        self.add_to_ret_template(req.table, req.db, ret["keys"], ret["error"], false)
+        self.add_to_ret_template(req.table, req.db, ret["keys"], ret["error"], False)
         req = MatchRequest(db="APPL_DB", table="MCLAG_FDB_TABLE", key_pattern=fdb_name, ns=self.ns)
         ret = self.match_engine.fetch(req)
-        self.add_to_ret_template(req.table, req.db, ret["keys"], ret["error"], false)
+        self.add_to_ret_template(req.table, req.db, ret["keys"], ret["error"], False)
 
     def init_asic_fdb_info(self, fdb_name):
         key_split = fdb_name.split(":",1)
