3bd91b58e107fd7af2054b6b80f76eb501434ebe
Renuka Manavalan <remanava@microsoft.com>
2022-01-18 00:13:17 +0000

    Handle failed restart

--- generic_config_updater/services_validator.py
+++ generic_config_updater/services_validator.py
@@ -5,6 +5,8 @@ logger = genericUpdaterLogging.get_logger(title="Service Validator")
 
 print_to_console = False
 
+PAUSE_AFTER_FAILED_RESTART = "7s"
+
 def set_verbose(verbose=False):
     global print_to_console, logger
 
@@ -17,8 +19,35 @@ def set_verbose(verbose=False):
 
 def _service_restart(svc_name):
     rc = os.system(f"systemctl restart {svc_name}")
-    logger.log(logger.LOG_PRIORITY_NOTICE,
-            f"Restarted {svc_name}", print_to_console)
+    if rc != 0:
+        # This failure is likely due to too many restarts
+        # Which is the reflection of patch sorter not grouping
+        # updates for keys effectively.
+        # Once the patch sorter is tuned, the following
+        # code would not be executed.
+        #
+        rc = os.system(f"systemctl reset-failed {svc_name}")
+        logger.log(logger.LOG_PRIORITY_ERROR, 
+                f"Service has been reset. rc={rc} Pause for {PAUSE_AFTER_FAILED_RESTART}",
+                print_to_console)
+
+        # Even with successful reset-failed, a pause is required, else the
+        # immediate restart was found to fail in manual tests.
+        # Hence add a pause.
+        # This pause would help even if the earlier reset-failed fails.
+        # Hence we ignore return code from reset-failed.
+        #
+        os.system(f"sleep {PAUSE_AFTER_FAILED_RESTART}")
+        rc = os.system(f"systemctl restart {svc_name}")
+
+    if rc == 0:
+        logger.log(logger.LOG_PRIORITY_NOTICE,
+                f"Restart succeeded for {svc_name}",
+                print_to_console)
+    else:
+        logger.log(logger.LOG_PRIORITY_ERROR,
+                f"Restart failed for {svc_name} rc={rc}",
+                print_to_console)
     return rc == 0
 
 
@@ -41,3 +70,7 @@ def vlan_validator(old_config, upd_config, keys):
     # No update to DHCP servers.
     return True
 
+
+def main_test():
+    for(i=0; i<10; ++i):
+        _service_restart("rsyslog-config")
