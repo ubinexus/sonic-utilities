b587264d239b65342af3b25be03f229cebb61832
tjchadaga <tchadaga@microsoft.com>
2022-02-12 00:25:28 +0000

    Migrate 10G ports during warm-reboot on 6100

--- scripts/db_migrator.py
+++ scripts/db_migrator.py
@@ -73,6 +73,7 @@ class DBMigrator():
         version_info = device_info.get_sonic_version_info()
         asic_type = version_info.get('asic_type')
         self.asic_type = asic_type
+        self.hwsku = device_info.get_hwsku()
 
         if asic_type == "mellanox":
             from mellanox_buffer_migrator import MellanoxBufferMigrator
@@ -124,6 +125,50 @@ class DBMigrator():
                 log.log_info('Migrating interface table for ' + key[0])
                 self.configDB.set_entry(table, key[0], data[key])
                 if_db.append(key[0])
+
+    def migrate_mgmt_ports_on_s6100(self):
+        '''
+        During warm-reboot, add back two 10G management ports which got removed from 6100
+        to ensure no change in bcm.config from older image
+        '''
+        if device_info.is_warm_restart_enabled('swss') == False:
+            log.log_notice("Skip migration on {}, warm-reboot flag not set".format(self.hwsku))
+            return True
+        
+        portCount = self.appDB.get(self.appDB.APPL_DB, 'PORT_TABLE:PortConfigDone', 'count')
+        if portCount == '':
+            log.log_notice("Port count not found - {}, skip migration".format(portCount))
+            return True
+
+        self.portCount = int(portCount)
+        if self.portCount > 64:
+            log.log_notice("Port count is {}, no migration needed".format(self.portCount))
+            return True        
+        
+        log.log_notice("Migrating management ports Ethernet64 & Ethernet65 to configDB for warm-reboot on {}".format(self.hwsku))
+        self.configDB.set_entry('PORT', 'Ethernet64', {'alias': 'tenGige1/1', 'description': 'tenGige1/1', 'index': '64', 'lanes': '129', 'mtu': '9100', 'pfc_asym': 'off', 'speed': '10000'})
+        self.configDB.set_entry('PORT', 'Ethernet65', {'alias': 'tenGige1/2', 'description': 'tenGige1/2', 'index': '65', 'lanes': '131', 'mtu': '9100', 'pfc_asym': 'off', 'speed': '10000'})
+
+        #Update APPL_DB with newly added ports
+        portList = ['Ethernet64', 'Ethernet65']
+        for portName in portList:
+            key = 'PORT_TABLE:' + portName
+            port = self.configDB.get_entry('PORT', portName)
+            if not port:
+                log.log_notice("Skipping migration for port {}".format(key))
+                continue
+
+            for field, value in port.items():
+                self.appDB.set(self.appDB.APPL_DB, key, field, value) 
+            self.appDB.set(self.appDB.APPL_DB, key, 'admin_status', 'down')
+            log.log_notice("Copied port {} to appdb".format(key))
+            self.portCount += 1
+
+        #Update port count in APPL_DB
+        self.appDB.set(self.appDB.APPL_DB, 'PORT_TABLE:PortConfigDone', 'count', str(self.portCount)) 
+        log.log_notice("Port count updated from {} to : {}".format(portCount, self.appDB.get(self.appDB.APPL_DB, 'PORT_TABLE:PortConfigDone', 'count')))
+
+        return True
 
     def migrate_intf_table(self):
         '''
@@ -666,6 +711,10 @@ class DBMigrator():
                 self.configDB.set_entry(init_cfg_table, key, new_cfg)
 
         self.migrate_copp_table()
+        if self.asic_type == "broadcom" and 'Force10-S6100' in self.hwsku:            
+            self.migrate_mgmt_ports_on_s6100()
+        else:
+            log.log_notice("Asic Type: {}, Hwsku: {}".format(self.asic_type, self.hwsku))
 
     def migrate(self):
         version = self.get_version()
