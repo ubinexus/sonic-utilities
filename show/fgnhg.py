8de000fbe9367494de61a2a2dc4f8844b24dccad
Kavin Kamaraj (kakamara) <kakamara@nettools1-eastus2.phx.gbl>
2022-03-23 00:55:08 +0000

    Remove all unneeded spacing

--- show/fgnhg.py
+++ show/fgnhg.py
@@ -26,15 +26,12 @@ def active_hops(nhg):
     table = []
     output_dict = {}
     ctx = click.get_current_context()
-
     try:
         table_keys = sorted(state_db.keys(state_db.STATE_DB, _hash))
     except Exception as e:
         ctx.fail("FG_ROUTE_TABLE does not exist!")
-
     if table_keys is None:
         ctx.fail("FG_ROUTE_TABLE does not exist!")
-
     if nhg is None:
         for nhg_prefix in table_keys:
             t_dict = state_db.get_all(state_db.STATE_DB, nhg_prefix)
@@ -44,13 +41,11 @@ def active_hops(nhg):
                     output_dict[nhg_prefix].append(nh_ip.split("@")[0])
                 else:
                     output_dict[nhg_prefix] = [nh_ip.split("@")[0]]
-
             nhg_prefix_report = (nhg_prefix.split("|")[1])
             formatted_nhps = ','.replace(',', '\n').join(output_dict[nhg_prefix])
             table.append([nhg_prefix_report, formatted_nhps])
 
         click.echo(tabulate(table, header, tablefmt="simple"))
-
     else:
         nhip_prefix_map = {}
         header = ["FG NHG Prefix", "Active Next Hops"]
@@ -61,7 +56,6 @@ def active_hops(nhg):
         alias_list = []
         nexthop_alias = {}
         output_list = []
-
         for nexthop, nexthop_metadata in fg_nhg_member_table.items():
             alias_list.append(nexthop_metadata['FG_NHG'])
             nexthop_alias[nexthop] = nexthop_metadata['FG_NHG']
@@ -71,7 +65,6 @@ def active_hops(nhg):
             for nhg_prefix in table_keys:
                 t_dict = state_db.get_all(state_db.STATE_DB, nhg_prefix)
                 vals = sorted(set([val for val in t_dict.values()]))
-
                 for nh_ip in vals:
                     nhip_prefix_map[nh_ip.split("@")[0]] = nhg_prefix
                    
@@ -83,11 +76,9 @@ def active_hops(nhg):
                 output_list = sorted(output_list)
             if not output_list:
                 ctx.fail ("FG_ROUTE table likely does not contain the required entries")
-
             nhg_prefix_report = nhip_prefix_map[output_list[0]].split("|")[1]
             formatted_output_list = ','.replace(',', '\n').join(output_list)
             table.append([nhg_prefix_report, formatted_output_list])
-
             click.echo(tabulate(table, header, tablefmt="simple"))
 
 @fgnhg.command()
@@ -107,36 +98,28 @@ def hash_view(nhg):
     output_dict = {}
     bank_dict = {}
     ctx = click.get_current_context()
-
     try:
         table_keys = sorted(state_db.keys(state_db.STATE_DB, _hash))
     except Exception as e: 
         ctx.fail("FG_ROUTE_TABLE does not exist!")
         click.echo(e)
-
     if table_keys is None:
         ctx.fail("FG_ROUTE_TABLE does not exist!")
-
     if nhg is None:
         for nhg_prefix in table_keys:
             bank_dict = {}
             t_dict = state_db.get_all(state_db.STATE_DB, nhg_prefix)
             vals = sorted(set([val for val in t_dict.values()]))
-
             for nh_ip in vals:
                 bank_ids = sorted([int(k) for k, v in t_dict.items() if v == nh_ip])
-
                 bank_ids = [str(x) for x in bank_ids]
-
                 if nhg_prefix in output_dict:
                     output_dict[nhg_prefix].append(nh_ip.split("@")[0])
                 else:
                     output_dict[nhg_prefix] = [nh_ip.split("@")[0]]
                 bank_dict[nh_ip.split("@")[0]] = bank_ids
-
             bank_dict = OrderedDict(sorted(bank_dict.items()))
             nhg_prefix_report = (nhg_prefix.split("|")[1])
-
             for nhip, val in bank_dict.items():
                 displayed_banks = []
                 bank_output = ""
@@ -150,15 +133,11 @@ def hash_view(nhg):
 
                     if (len(str(bankid)) == 3):
                         displayed_banks.append(str(bankid))
-
                 for i in range (0, len(displayed_banks), 8):
                     bank_output = bank_output + " ".join(displayed_banks[i:i+8]) + "\n" 
-
                 bank_output = bank_output + "\n"
                 table.append([nhg_prefix_report, nhip, bank_output])
-
         click.echo(tabulate(table, header, tablefmt="simple"))
-
     else:
         header = ["FG NHG Prefix", "Next Hop", "Hash buckets"]
         try:
@@ -166,17 +145,13 @@ def hash_view(nhg):
         except Exception as e: 
             ctx.fail("FG_NHG_MEMBER entries not present in config_db")
             click.echo(e)
-
         alias_list = []
         nexthop_alias = {}
-
         for nexthop, nexthop_metadata in fg_nhg_member_table.items():
             alias_list.append(nexthop_metadata['FG_NHG'])
             nexthop_alias[nexthop] = nexthop_metadata['FG_NHG']
-
         if nhg not in alias_list:
             ctx.fail ("Please provide a valid NHG alias")
-
         else:
             nhip_prefix_map = {}
             for nhg_prefix in table_keys:
@@ -187,13 +162,11 @@ def hash_view(nhg):
                     bank_ids = sorted([int(k) for k, v in t_dict.items() if v == nh_ip])
                     nhip_prefix_map[nh_ip.split("@")[0]] = nhg_prefix
                     bank_ids = [str(x) for x in bank_ids]
-
                     if nhg_prefix in output_dict:
                         output_dict[nhg_prefix].append(nh_ip.split("@")[0])
                     else:
                         output_dict[nhg_prefix] = [nh_ip.split("@")[0]]
                     bank_dict[nh_ip.split("@")[0]] = bank_ids
-
                 bank_dict = OrderedDict(sorted(bank_dict.items()))
                 output_bank_dict = {}
                 for nexthop, banks  in bank_dict.items():
@@ -202,11 +175,8 @@ def hash_view(nhg):
                             output_bank_dict[nexthop] = banks
                     else:
                         ctx.fail ("state_db and config_db have FGNHG prefix config mismatch. Check device config!");
-
                 nhg_prefix_report = nhip_prefix_map[list(bank_dict.keys())[0]].split("|")[1]
-
                 output_bank_dict = OrderedDict(sorted(output_bank_dict.items())) 
-
                 for nhip, val in output_bank_dict.items():
                     bank_output = ""
                     displayed_banks = []
@@ -220,10 +190,7 @@ def hash_view(nhg):
 
                         if (len(str(bankid)) == 3):
                             displayed_banks.append(str(bankid))
-
                     for i in range (0, len(displayed_banks), 8):
                         bank_output = bank_output + " ".join(displayed_banks[i:i+8]) + "\n" 
-
                     table.append([nhg_prefix_report, nhip, bank_output])
-
             click.echo(tabulate(table, header, tablefmt="simple"))
