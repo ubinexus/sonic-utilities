5aa74a3e1dbccef131cc35a924eb2e7c042961ee
Stepan Blyschak <stepanb@nvidia.com>
2022-05-04 18:41:03 +0300

    [sonic-bootchart] add sonic-bootchart
    Signed-off-by: Stepan Blyschak <stepanb@nvidia.com>

---
+++ tests/sonic_bootchart_test.py
@@ -0,0 +1,81 @@
+import os
+import subprocess
+import pytest
+from click.testing import CliRunner
+from unittest.mock import patch, Mock
+import utilities_common
+import imp
+
+sonic_bootchart = imp.load_source('sonic-bootchart', 'scripts/sonic-bootchart')
+
+BOOTCHART_OUTPUT_FILES = [
+    os.path.join(sonic_bootchart.BOOTCHART_DEFAULT_OUTPUT_DIR, "bootchart-20220504-1040.svg"),
+    os.path.join(sonic_bootchart.BOOTCHART_DEFAULT_OUTPUT_DIR, "bootchart-20220504-1045.svg"),
+]
+
+@pytest.fixture(autouse=True)
+def setup(fs):
+    # create required file for bootchart installation check
+    fs.create_file(sonic_bootchart.SYSTEMD_BOOTCHART)
+    fs.create_file(sonic_bootchart.BOOTCHART_CONF)
+    for bootchart_output_file in BOOTCHART_OUTPUT_FILES:
+        fs.create_file(bootchart_output_file)
+
+    with open(sonic_bootchart.BOOTCHART_CONF, 'w') as config_file:
+        config_file.write("""
+        [Bootchart]
+        Samples=500
+        Frequency=25
+        """)
+
+    # pass the root user check
+    with patch("os.geteuid") as mock:
+        mock.return_value = 0
+        yield
+
+
+@patch("utilities_common.cli.run_command")
+class TestSonicBootchart:
+    def test_enable(self, mock_run_command):
+        runner = CliRunner()
+        result = runner.invoke(sonic_bootchart.cli.commands['enable'], [])
+        assert not result.exit_code
+        mock_run_command.assert_called_with("systemctl enable systemd-bootchart", display_cmd=True)
+
+    def test_disable(self, mock_run_command):
+        runner = CliRunner()
+        result = runner.invoke(sonic_bootchart.cli.commands['disable'], [])
+        assert not result.exit_code
+        mock_run_command.assert_called_with("systemctl disable systemd-bootchart", display_cmd=True)
+
+    def test_config_show(self, mock_run_command):
+        def run_command_side_effect(command, **kwargs):
+            if "is-enabled":
+                return "enabled"
+            elif "is-active":
+                return "active"
+            else:
+                raise Exception("unknown command")
+
+        mock_run_command.side_effect = run_command_side_effect
+
+        runner = CliRunner()
+        result = runner.invoke(sonic_bootchart.cli.commands['show'], [])
+        assert not result.exit_code
+        assert result.output == \
+                "Status    Operational Status      Samples    Frequency  Output\n"                               \
+                "--------  --------------------  ---------  -----------  ------------------------------------\n" \
+                "enabled   enabled                     500           25  /run/log/bootchart-20220504-1040.svg\n" \
+                "                                                        /run/log/bootchart-20220504-1045.svg\n"
+
+        result = runner.invoke(sonic_bootchart.cli.commands["config"], ["--samples", "100", "--frequency", "50"])
+        assert not result.exit_code
+
+        result = runner.invoke(sonic_bootchart.cli.commands['show'], [])
+        assert not result.exit_code
+        assert result.output == \
+                "Status    Operational Status      Samples    Frequency  Output\n"                               \
+                "--------  --------------------  ---------  -----------  ------------------------------------\n" \
+                "enabled   enabled                     100           50  /run/log/bootchart-20220504-1040.svg\n" \
+                "                                                        /run/log/bootchart-20220504-1045.svg\n"
+
