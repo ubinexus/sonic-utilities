36634f37be7af4f8f6237e2c6dd5235639a8a752
anilkpan <47642449+anilkpan@users.noreply.github.com>
2022-05-18 18:29:50 -0700

    Update portchannel_test.py

--- tests/portchannel_test.py
+++ tests/portchannel_test.py
@@ -163,32 +163,32 @@ class TestPortChannel(object):
         obj = {'db':db.cfgdb}
 
         # add a portchannel
-        result = runner.invoke(config.config.commands["portchannel"].commands["add"], ["PortChannel0005"], obj=obj)
+        result = runner.invoke(config.config.commands["portchannel"].commands["add"], ["PortChannel1005"], obj=obj)
         print(result.exit_code)
         print(result.output)
         assert result.exit_code == 0
 
         # add portchannel to a vlan
-        result = runner.invoke(config.config.commands["vlan"].commands["member"].commands["add"], ["2000", "PortChannel0005"], obj=obj)
+        result = runner.invoke(config.config.commands["vlan"].commands["member"].commands["add"], ["2000", "PortChannel1005"], obj=obj)
         print(result.exit_code)
         print(result.output)
         assert result.exit_code == 0
 
         # try to delete the portchannel
-        result = runner.invoke(config.config.commands["portchannel"].commands["del"], ["PortChannel0005"], obj=obj)
+        result = runner.invoke(config.config.commands["portchannel"].commands["del"], ["PortChannel1005"], obj=obj)
         print(result.exit_code)
         print(result.output)
         assert result.exit_code != 0
         assert "PortChannel0005 has vlan Vlan2000 configured, remove vlan membership to proceed" in result.output
 
         # remove portchannel from vlan
-        result = runner.invoke(config.config.commands["vlan"].commands["member"].commands["del"], ["2000", "PortChannel0005"], obj=obj)
+        result = runner.invoke(config.config.commands["vlan"].commands["member"].commands["del"], ["2000", "PortChannel1005"], obj=obj)
         print(result.exit_code)
         print(result.output)
         assert result.exit_code == 0
 
         # delete the portchannel
-        result = runner.invoke(config.config.commands["portchannel"].commands["del"], ["PortChannel0005"], obj=obj)
+        result = runner.invoke(config.config.commands["portchannel"].commands["del"], ["PortChannel1005"], obj=obj)
         print(result.exit_code)
         print(result.output)
         assert result.exit_code == 0
