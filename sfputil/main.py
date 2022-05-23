7d0b9d8dcd584a45bcf271b9a797faf5e19e3e01
Chuan Qin (QINCHUAN) <QINCHUAN@ame.gbl>
2022-05-23 15:29:25 -0700

    add back application_advertisement

--- sfputil/main.py
+++ sfputil/main.py
@@ -326,9 +326,6 @@ def convert_sfp_info_to_output_string(sfp_info_dict):
                 pass
             elif key == 'specification_compliance':
                 output += '{}{}: {}\n'.format(indent, QSFP_DD_DATA_MAP[key], sfp_info_dict[key])
-
-            elif key == 'application_advertisement':
-                pass
             elif key == 'supported_max_tx_power' or key == 'supported_min_tx_power':
                 if key in sfp_info_dict:  # C-CMIS compliant / coherent modules
                     output += '{}{}: {}dBm\n'.format(indent, QSFP_DD_DATA_MAP[key], sfp_info_dict[key])
