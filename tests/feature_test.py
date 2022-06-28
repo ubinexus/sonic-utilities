c0ca0a395957121bf27cae0b4546ae663d4fe7e3
Yong Zhao <yozhao@microsoft.com>
2022-06-28 13:34:06 -0700

    [show/config] Configure and show commands to enable high memory threshold/alerting option.
    Signed-off-by: Yong Zhao <yozhao@microsoft.com>

--- tests/feature_test.py
+++ tests/feature_test.py
@@ -10,102 +10,102 @@ from utilities_common.db import Db
 from swsscommon import swsscommon
 
 show_feature_status_output="""\
-Feature     State           AutoRestart     SetOwner
-----------  --------------  --------------  ----------
-bgp         enabled         enabled         local
-database    always_enabled  always_enabled  local
-dhcp_relay  enabled         enabled         kube
-lldp        enabled         enabled         kube
-nat         enabled         enabled         local
-pmon        enabled         enabled         kube
-radv        enabled         enabled         kube
-restapi     disabled        enabled         local
-sflow       disabled        enabled         local
-snmp        enabled         enabled         kube
-swss        enabled         enabled         local
-syncd       enabled         enabled         local
-teamd       enabled         enabled         local
-telemetry   enabled         enabled         kube
+Feature     State           AutoRestart     HighMemAlert      MemThreshold    SetOwner
+----------  --------------  --------------  ----------------  --------------  ----------
+bgp         enabled         enabled         enabled           1073741824      local
+database    always_enabled  always_enabled  enabled           1073741824      local
+dhcp_relay  enabled         enabled         enabled           1073741824      kube
+lldp        enabled         enabled         enabled           1073741824      kube
+nat         enabled         enabled         enabled           1073741824      local
+pmon        enabled         enabled         enabled           1073741824      kube
+radv        enabled         enabled         enabled           1073741824      kube
+restapi     disabled        enabled         enabled           1073741824      local
+sflow       disabled        enabled         enabled           1073741824      local
+snmp        enabled         enabled         enabled           1073741824      kube
+swss        enabled         enabled         enabled           1073741824      local
+syncd       enabled         enabled         enabled           1073741824      local
+teamd       enabled         enabled         enabled           1073741824      local
+telemetry   enabled         enabled         enabled           1073741824      kube
 """
 
 show_feature_status_output_with_remote_mgmt="""\
-Feature     State           AutoRestart     SystemState    UpdateTime           ContainerId    Version       SetOwner    CurrentOwner    RemoteState
-----------  --------------  --------------  -------------  -------------------  -------------  ------------  ----------  --------------  -------------
-bgp         enabled         enabled                                                                          local
-database    always_enabled  always_enabled                                                                   local
-dhcp_relay  enabled         enabled                                                                          kube
-lldp        enabled         enabled                                                                          kube
-nat         enabled         enabled                                                                          local
-pmon        enabled         enabled                                                                          kube
-radv        enabled         enabled                                                                          kube
-restapi     disabled        enabled                                                                          local
-sflow       disabled        enabled                                                                          local
-snmp        enabled         enabled         up             2020-11-12 23:32:56  aaaabbbbcccc   20201230.100  kube        kube            kube
-swss        enabled         enabled                                                                          local
-syncd       enabled         enabled                                                                          local
-teamd       enabled         enabled                                                                          local
-telemetry   enabled         enabled                                                                          kube
+Feature     State           AutoRestart     HighMemAlert      MemThreshold    SystemState    UpdateTime           ContainerId    Version       SetOwner    CurrentOwner    RemoteState
+----------  --------------  --------------  ----------------  --------------  -------------  -------------------  -------------  ------------  ----------  --------------  -------------
+bgp         enabled         enabled         enabled           1073741824                                                                       local
+database    always_enabled  always_enabled  enabled           1073741824                                                                       local
+dhcp_relay  enabled         enabled         enabled           1073741824                                                                       kube
+lldp        enabled         enabled         enabled           1073741824                                                                       kube
+nat         enabled         enabled         enabled           1073741824                                                                       local
+pmon        enabled         enabled         enabled           1073741824                                                                       kube
+radv        enabled         enabled         enabled           1073741824                                                                       kube
+restapi     disabled        enabled         enabled           1073741824                                                                       local
+sflow       disabled        enabled         enabled           1073741824                                                                       local
+snmp        enabled         enabled         enabled           1073741824      up             2020-11-12 23:32:56  aaaabbbbcccc   20201230.100  kube        kube            kube
+swss        enabled         enabled         enabled           1073741824                                                                       local
+syncd       enabled         enabled         enabled           1073741824                                                                       local
+teamd       enabled         enabled         enabled           1073741824                                                                       local
+telemetry   enabled         enabled         enabled           1073741824                                                                       kube
 """
 
 show_feature_config_output="""\
-Feature     State     AutoRestart
-----------  --------  -------------
-bgp         enabled   enabled
-database    enabled   disabled
-dhcp_relay  enabled   enabled
-lldp        enabled   enabled
-nat         enabled   enabled
-pmon        enabled   enabled
-radv        enabled   enabled
-restapi     disabled  enabled
-sflow       disabled  enabled
-snmp        enabled   enabled
-swss        enabled   enabled
-syncd       enabled   enabled
-teamd       enabled   enabled
-telemetry   enabled   enabled
+Feature     State     AutoRestart    HighMemAlert      MemThreshold
+----------  --------  -------------  ----------------  --------------
+bgp         enabled   enabled        enabled           1073741824
+database    enabled   disabled       enabled           1073741824
+dhcp_relay  enabled   enabled        enabled           1073741824
+lldp        enabled   enabled        enabled           1073741824
+nat         enabled   enabled        enabled           1073741824
+pmon        enabled   enabled        enabled           1073741824
+radv        enabled   enabled        enabled           1073741824
+restapi     disabled  enabled        enabled           1073741824
+sflow       disabled  enabled        enabled           1073741824
+snmp        enabled   enabled        enabled           1073741824
+swss        enabled   enabled        enabled           1073741824
+syncd       enabled   enabled        enabled           1073741824
+teamd       enabled   enabled        enabled           1073741824
+telemetry   enabled   enabled        enabled           1073741824
 """
 
 show_feature_config_output_with_remote_mgmt="""\
-Feature     State           AutoRestart     Owner
-----------  --------------  --------------  -------
-bgp         enabled         enabled         local
-database    always_enabled  always_enabled  local
-dhcp_relay  enabled         enabled         kube
-lldp        enabled         enabled         kube
-nat         enabled         enabled         local
-pmon        enabled         enabled         kube
-radv        enabled         enabled         kube
-restapi     disabled        enabled         local
-sflow       disabled        enabled         local
-snmp        enabled         enabled         kube
-swss        enabled         enabled         local
-syncd       enabled         enabled         local
-teamd       enabled         enabled         local
-telemetry   enabled         enabled         kube
+Feature     State           AutoRestart     HighMemAlert      MemThreshold    Owner
+----------  --------------  --------------  ----------------  --------------  -------
+bgp         enabled         enabled         enabled           1073741824      local
+database    always_enabled  always_enabled  enabled           1073741824      local
+dhcp_relay  enabled         enabled         enabled           1073741824      kube
+lldp        enabled         enabled         enabled           1073741824      kube
+nat         enabled         enabled         enabled           1073741824      local
+pmon        enabled         enabled         enabled           1073741824      kube
+radv        enabled         enabled         enabled           1073741824      kube
+restapi     disabled        enabled         enabled           1073741824      local
+sflow       disabled        enabled         enabled           1073741824      local
+snmp        enabled         enabled         enabled           1073741824      kube
+swss        enabled         enabled         enabled           1073741824      local
+syncd       enabled         enabled         enabled           1073741824      local
+teamd       enabled         enabled         enabled           1073741824      local
+telemetry   enabled         enabled         enabled           1073741824      kube
 """
 
 show_feature_bgp_status_output="""\
-Feature    State    AutoRestart    SetOwner
----------  -------  -------------  ----------
-bgp        enabled  enabled        local
+Feature    State    AutoRestart    HighMemAlert    MemThreshold    SetOwner
+---------  -------  -------------  --------------  --------------  ---------
+bgp        enabled  enabled        enabled         1073741824      local
 """
 
 show_feature_bgp_disabled_status_output="""\
-Feature    State     AutoRestart    SetOwner
----------  --------  -------------  ----------
-bgp        disabled  enabled        local
+Feature    State     AutoRestart   HighMemAlert   MemThreshold    SetOwner
+---------  --------  ------------  -------------  --------------  ----------
+bgp        disabled  enabled       enabled        1073741824      local
 """
 show_feature_snmp_config_owner_output="""\
-Feature    State    AutoRestart    Owner    fallback
----------  -------  -------------  -------  ----------
-snmp       enabled  enabled        local    true
+Feature    State    AutoRestart    HighMemAlert   MemThreshold    Owner    fallback
+---------  -------  -------------  -------------  --------------  -------  ----------
+snmp       enabled  enabled        enabled        1073741824      local    true
 """
 
 show_feature_snmp_config_fallback_output="""\
-Feature    State    AutoRestart    Owner    fallback
----------  -------  -------------  -------  ----------
-snmp       enabled  enabled        kube     false
+Feature    State    AutoRestart    HighMemAlert   MemThreshold    Owner    fallback
+---------  -------  -------------  -------------  --------------  -------  ----------
+snmp       enabled  enabled        enabled        1073741824      kube     false
 """
 
 show_feature_autorestart_output="""\
@@ -133,7 +133,6 @@ Feature    AutoRestart
 bgp        enabled
 """
 
-
 show_feature_bgp_disabled_autorestart_output="""\
 Feature    AutoRestart
 ---------  -------------
@@ -151,9 +150,61 @@ Feature    AutoRestart
 ---------  --------------
 database   always_enabled
 """
+
 config_feature_bgp_inconsistent_state_output="""\
 Feature 'bgp' state is not consistent across namespaces
 """
+
+show_feature_high_mem_alert="""\
+Feature     HighMemAlert
+----------  ----------------
+bgp         enabled
+database    enabled
+dhcp_relay  enabled
+lldp        enabled
+nat         enabled
+pmon        enabled
+radv        enabled
+restapi     enabled
+sflow       enabled
+snmp        enabled
+swss        enabled
+syncd       enabled
+teamd       enabled
+telemetry   enabled
+"""
+
+show_feature_high_mem_alert_lldp="""\
+Feature    HighMemRestart
+---------  ----------------
+lldp       enabled
+"""
+
+show_feature_mem_threshold="""\
+Feature     MemThreshold
+----------  --------------
+bgp         1073741824
+database    1073741824
+dhcp_relay  1073741824
+lldp        1073741824
+nat         1073741824
+pmon        1073741824
+radv        1073741824
+restapi     1073741824
+sflow       1073741824
+snmp        1073741824
+swss        1073741824
+syncd       1073741824
+teamd       1073741824
+telemetry   1073741824
+"""
+
+show_feature_mem_threshold_lldp="""\
+Feature    MemThreshold
+---------  --------------
+lldp       1073741824
+"""
+
 config_feature_bgp_inconsistent_autorestart_output="""\
 Feature 'bgp' auto-restart is not consistent across namespaces
 """
@@ -237,6 +288,52 @@ class TestFeature(object):
         print(result.output)
         assert result.exit_code == 0
         assert result.output == show_feature_autorestart_output
+
+    def test_show_feature_high_mem_restart(self, get_cmd_module):
+        (config, show) = get_cmd_module
+        runner = CliRunner()
+        db = Db()
+
+        result = runner.invoke(show.cli.commands["feature"].commands["high_mem_restart"], [])
+        print(result.exit_code)
+        print(result.output)
+        assert result.exit_code == 0
+        assert result.output == show_feature_high_mem_alert
+
+        result = runner.invoke(show.cli.commands["feature"].commands["high_mem_restart"], ["lldp"])
+        print(result.exit_code)
+        print(result.output)
+        print(show_feature_high_mem_alert_lldp)
+        assert result.exit_code == 0
+        assert result.output == show_feature_high_mem_alert_lldp
+
+        result = runner.invoke(show.cli.commands["feature"].commands["high_mem_restart"], ["not_existing_container"])
+        print(result.exit_code)
+        print(result.output)
+        assert result.exit_code == 3
+
+    def test_show_feature_mem_threshold(self, get_cmd_module):
+        (config, show) = get_cmd_module
+        runner = CliRunner()
+        db = Db()
+
+        result = runner.invoke(show.cli.commands["feature"].commands["mem_threshold"], [])
+        print(result.exit_code)
+        print(result.output)
+        assert result.exit_code == 0
+        assert result.output == show_feature_mem_threshold
+
+        result = runner.invoke(show.cli.commands["feature"].commands["mem_threshold"], ["lldp"])
+        print(result.exit_code)
+        print(result.output)
+        assert result.exit_code == 0
+        assert result.output == show_feature_mem_threshold_lldp
+
+        result = runner.invoke(show.cli.commands["feature"].commands["mem_threshold"], ["non_existing_container"])
+        print(result.exit_code)
+        print(result.output)
+        assert result.exit_code == 5
+
 
     def test_fail_autorestart(self, get_cmd_module):
         (config, show) = get_cmd_module
