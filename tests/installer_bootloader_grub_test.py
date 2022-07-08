61c222c946d6f86a863fafa0403a0209d7eec74e
xumia <xumia@microsoft.com>
2022-07-08 05:29:38 +0000

    Fix mock issue

--- tests/installer_bootloader_grub_test.py
+++ tests/installer_bootloader_grub_test.py
@@ -1,6 +1,5 @@
 import os
 import shutil
-import sonic_installer
 from unittest.mock import Mock, patch
 
 # Import test module
@@ -27,7 +26,7 @@ def test_remove_image(open_patch, run_command_patch, re_search_patch):
     args, _ = args_list[0]
     assert exp_image_path in args[0]
 
-@patch("sonic_installer.common.HOST_PATH", os.path.join(os.path.dirname(os.path.abspath(__file__)), 'installer_bootloader_input/_tmp_host'))
+@patch("sonic_installer.bootloader.grub.HOST_PATH", os.path.join(os.path.dirname(os.path.abspath(__file__)), 'installer_bootloader_input/_tmp_host'))
 def test_set_fips_grub():
     # Prepare the grub.cfg in the _tmp_host folder
     current_path = os.path.dirname(os.path.abspath(__file__))
@@ -37,8 +36,6 @@ def test_set_fips_grub():
     tmp_grub_config = os.path.join(tmp_grub_path, 'grub.cfg')
     os.makedirs(tmp_grub_path, exist_ok=True)
     shutil.copy(grub_config, tmp_grub_path)
-
-    sonic_installer.common.HOST_PATH = tmp_host_path
 
     image = 'SONiC-OS-internal-202205.57377412-84a9a7f11b'
     bootloader = grub.GrubBootloader()
