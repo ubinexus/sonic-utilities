f78b7b18d5263765d8e8abc07c6f0bd16064b072
pettershao-ragilenetworks <81281940+pettershao-ragilenetworks@users.noreply.github.com>
2022-09-29 15:48:56 +0800

    Create config_int_mtu_test.py

---
+++ tests/config_int_mtu_test.py
@@ -0,0 +1,25 @@
+import pytest
+import config.main as config
+
+class TestConfigInterfaceMtu(object):
+    def test_interface_mtu_check():
+        result = runner.invoke(
+            config.config.commands["interface"].commands["mtu"],
+            ["Ethernet0", "68"])
+        assert result.exit_code != 0
+        
+        result = runner.invoke(
+            config.config.commands["interface"].commands["mtu"],
+            ["Ethernet0", "9216"])
+        assert result.exit_code != 0
+        
+    def test_interface_invalid_mtu_check():
+        result = runner.invoke(
+            config.config.commands["interface"].commands["mtu"],
+            ["Ethernet0", "67"])
+        assert not result.exit_code != 0
+        
+        result = runner.invoke(
+            config.config.commands["interface"].commands["mtu"],
+            ["Ethernet0", "9216"])
+        assert not result.exit_code != 0
