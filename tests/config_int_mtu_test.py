394a6411cec2f37ca65da159694ad22c5fe23fcb
pettershao-ragilenetworks <81281940+pettershao-ragilenetworks@users.noreply.github.com>
2022-09-29 18:17:39 +0800

    Update config_int_mtu_test.py

--- tests/config_int_mtu_test.py
+++ tests/config_int_mtu_test.py
@@ -5,21 +5,21 @@ class TestConfigInterfaceMtu(object):
     def test_interface_mtu_check():
         result = runner.invoke(
             config.config.commands["interface"].commands["mtu"],
-            ["Ethernet0", 68])
+            ["Ethernet0", "68"])
         assert result.exit_code != 0
         
         result1 = runner.invoke(
             config.config.commands["interface"].commands["mtu"],
-            ["Ethernet0", 9216])
+            ["Ethernet0", "9216"])
         assert result1.exit_code != 0
         
     def test_interface_invalid_mtu_check():
         result = runner.invoke(
             config.config.commands["interface"].commands["mtu"],
-            ["Ethernet0", 67])
+            ["Ethernet0", "67"])
         assert not "Error: Invalid value" in result.output
         
         result1 = runner.invoke(
             config.config.commands["interface"].commands["mtu"],
-            ["Ethernet0", 9217])
+            ["Ethernet0", "9217"])
         assert not "Error: Invalid value" in result1.output
