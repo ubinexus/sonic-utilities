18fce6ddb1cf5659989f5a25dfdd13d289504288
pettershao-ragilenetworks <81281940+pettershao-ragilenetworks@users.noreply.github.com>
2022-09-30 10:42:41 +0800

    Update config_int_mtu_test.py

--- tests/config_int_mtu_test.py
+++ tests/config_int_mtu_test.py
@@ -20,8 +20,7 @@ class TestConfigInterfaceMtu(object):
         db = Db()
         result = runner.invoke(config.config.commands["interface"].commands["mtu"],
             ["Ethernet0", "67"], obj=db)
-        assert not 'Usage: mtu [OPTIONS] <interface_name> <interface_mtu>\nTry "mtu --help" for help.\n\nError: Invalid value for "<interface_mtu>": 67 is not in the valid range of 68 to 9216.\n' in result.output
-
+        assert result.exit_code == 0
         result1 = runner.invoke(config.config.commands["interface"].commands["mtu"],
             ["Ethernet0", "9217"], obj=db)
-        assert not 'Usage: mtu [OPTIONS] <interface_name> <interface_mtu>\nTry "mtu --help" for help.\n\nError: Invalid value for "<interface_mtu>": 9217 is not in the valid range of 68 to 9216.\n' in result1.output
+        assert result1.exit_code == 0
