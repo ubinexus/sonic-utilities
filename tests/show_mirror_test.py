2ec61c8c272b2db4019ecf9e787fd91dfa6b5fda
pettershao-ragilenetworks <81281940+pettershao-ragilenetworks@users.noreply.github.com>
2022-10-10 17:10:38 +0800

    Create show_mirror_test.py

---
+++ tests/show_mirror_test.py
@@ -0,0 +1,41 @@
+import os
+import sys
+from click.testing import CliRunner
+from swsscommon.swsscommon import SonicV2Connector
+from utilities_common.db import Db
+
+import show.main as show
+
+test_path = os.path.dirname(os.path.abspath(__file__))
+mock_db_path = os.path.join(test_path, "mirror_input")
+
+class TestShowMirror(object):
+    @classmethod
+    def setup_class(cls):
+        print("SETUP")
+        os.environ["UTILITIES_UNIT_TESTING"] = "1"
+
+    def test_mirror_show(self):
+        from .mock_tables import dbconnector
+        jsonfile_config = os.path.join(mock_db_path, "config_db")
+        dbconnector.dedicated_dbs['CONFIG_DB'] = jsonfile_config
+        runner = CliRunner()
+        db = Db()
+        expected_output = """\
+ERSPAN Sessions
+Name    Status    SRC IP    DST IP    GRE    DSCP    TTL    Queue    Policer    Monitor Port    SRC Port    Direction
+------  --------  --------  --------  -----  ------  -----  -------  ---------  --------------  ----------  -----------
+
+SPAN Sessions
+Name       Status    DST Port    SRC Port    Direction    Queue    Policer
+---------  --------  ----------  ----------  -----------  -------  ---------
+session1   active    Ethernet30  Ethernet40  both
+session11  active    Ethernet9   Ethernet10  rx
+session15  active    Ethernet2   Ethernet3   tx
+session2   active    Ethernet7   Ethernet8   both
+"""
+       
+        result = runner.invoke(show.cli.commands['mirror_session'], [], obj=db)
+        dbconnector.dedicated_dbs = {}
+        assert result.exit_code == 0
+        assert result.output == expected_output
