31d28c099ff6c2a602cf07be7e7283b5bf1913e5
isabelmsft <isabel.li@microsoft.com>
2022-10-11 00:20:32 +0000

    Refactor decorator based on Hua's feedback

--- config/validated_config_db_connector.py
+++ config/validated_config_db_connector.py
@@ -7,63 +7,69 @@ from generic_config_updater.generic_updater import GenericUpdater, ConfigFormat
 from generic_config_updater.gu_common import EmptyTableError, genericUpdaterLogging
 from swsscommon.swsscommon import ConfigDBConnector
 
-def ValidatedConfigDBConnector(config_db_connector):
-    yang_enabled = device_info.is_yang_config_validation_enabled(config_db_connector) 
-    if not yang_enabled:
-        return config_db_connector
-
-    validated_config_db_connector = ConfigDBConnector()
-    validated_config_db_connector.connect()
-    validated_config_db_connector.set_entry = types.MethodType(validated_set_entry, validated_config_db_connector)
-    validated_config_db_connector.delete_table = types.MethodType(validated_delete_table, validated_config_db_connector)
-    return validated_config_db_connector
-
-def make_path_value_jsonpatch_compatible(table, key, value):
-    if type(key) == tuple:
-        path = JsonPointer.from_parts([table, '|'.join(key)]).path
-    else:
-        path = JsonPointer.from_parts([table, key]).path
-    if value == {"NULL" : "NULL"}:
-        value = {}
-    return path, value
-
-def create_gcu_patch(op, table, key=None, value=None):
-    if key:
-        path, value = make_path_value_jsonpatch_compatible(table, key, value)
-    else: 
-        path = "/{}".format(table)
-
-    gcu_json_input = []
-    gcu_json = {"op": "{}".format(op),
-                "path": "{}".format(path)}
-    if op == "add":
-        gcu_json["value"] = value
-
-    gcu_json_input.append(gcu_json)
-    gcu_patch = jsonpatch.JsonPatch(gcu_json_input)
-    return gcu_patch
-
-def validated_delete_table(self, table):
-    gcu_patch = create_gcu_patch("remove", table)
-    format = ConfigFormat.CONFIGDB.name
-    config_format = ConfigFormat[format.upper()]
-    try:
-        GenericUpdater().apply_patch(patch=gcu_patch, config_format=config_format, verbose=False, dry_run=False, ignore_non_yang_tables=False, ignore_paths=None)
-    except ValueError as e:
-        logger = genericUpdaterLogging.get_logger(title="Patch Applier", print_all_to_console=True)
-        logger.log_notice("Unable to remove entry, as doing so will result in invalid config. Error: {}".format(e))
-
-def validated_set_entry(self, table, key, value):
-    if value is not None:
-        op = "add"
-    else:
-        op = "remove"
+class ValidatedConfigDBConnector(object):
     
-    gcu_patch = create_gcu_patch(op, table, key, value)
-    format = ConfigFormat.CONFIGDB.name
-    config_format = ConfigFormat[format.upper()]
+    def __init__(self, config_db_connector):
+        self.connector = config_db_connector
 
-    try:
-        GenericUpdater().apply_patch(patch=gcu_patch, config_format=config_format, verbose=False, dry_run=False, ignore_non_yang_tables=False, ignore_paths=None)
-    except EmptyTableError:
-        validated_delete_table(self, table)
+    def __getattr__(self, name):
+        config_db_connector = ConfigDBConnector()
+        config_db_connector.connect()
+        yang_enabled = device_info.is_yang_config_validation_enabled(config_db_connector)
+
+        if not yang_enabled:
+            return self.connector.__getattribute__(name)
+        if name == "set_entry":
+            return validated_set_entry
+        if name == "delete_table":
+            return validated_delete_table
+
+    def make_path_value_jsonpatch_compatible(table, key, value):
+        if type(key) == tuple:
+            path = JsonPointer.from_parts([table, '|'.join(key)]).path
+        else:
+            path = JsonPointer.from_parts([table, key]).path
+        if value == {"NULL" : "NULL"}:
+            value = {}
+        return path, value
+
+    def create_gcu_patch(op, table, key=None, value=None):
+        if key:
+            path, value = make_path_value_jsonpatch_compatible(table, key, value)
+        else: 
+            path = "/{}".format(table)
+
+        gcu_json_input = []
+        gcu_json = {"op": "{}".format(op),
+                    "path": "{}".format(path)}
+        if op == "add":
+            gcu_json["value"] = value
+
+        gcu_json_input.append(gcu_json)
+        gcu_patch = jsonpatch.JsonPatch(gcu_json_input)
+        return gcu_patch
+
+    def validated_delete_table(self, table):
+        gcu_patch = create_gcu_patch("remove", table)
+        format = ConfigFormat.CONFIGDB.name
+        config_format = ConfigFormat[format.upper()]
+        try:
+            GenericUpdater().apply_patch(patch=gcu_patch, config_format=config_format, verbose=False, dry_run=False, ignore_non_yang_tables=False, ignore_paths=None)
+        except ValueError as e:
+            logger = genericUpdaterLogging.get_logger(title="Patch Applier", print_all_to_console=True)
+            logger.log_notice("Unable to remove entry, as doing so will result in invalid config. Error: {}".format(e))
+
+    def validated_set_entry(self, table, key, value):
+        if value is not None:
+            op = "add"
+        else:
+            op = "remove"
+    
+        gcu_patch = create_gcu_patch(op, table, key, value)
+        format = ConfigFormat.CONFIGDB.name
+        config_format = ConfigFormat[format.upper()]
+
+        try:
+            GenericUpdater().apply_patch(patch=gcu_patch, config_format=config_format, verbose=False, dry_run=False, ignore_non_yang_tables=False, ignore_paths=None)
+        except EmptyTableError:
+            validated_delete_table(self, table)
