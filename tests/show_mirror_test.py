db66335f075e1c0ebfb49b4f609f419ed32a29e3
pettershao-ragilenetworks <81281940+pettershao-ragilenetworks@users.noreply.github.com>
2022-10-14 13:47:20 +0800

    Update show_mirror_test.py

--- tests/show_mirror_test.py
+++ tests/show_mirror_test.py
@@ -1,31 +1,28 @@
 import os
 import sys
-from click.testing import CliRunner
+import imp
 from swsscommon.swsscommon import SonicV2Connector
+from click.testing import CliRunner
 from utilities_common.db import Db
 
-from .utils import get_result_and_return_code
-
-import show.main as show
+import mock_tables.dbconnector
+import acl_loader.main as acl_loader_show
 
 test_path = os.path.dirname(os.path.abspath(__file__))
 mock_db_path = os.path.join(test_path, "mirror_input")
-
-modules_path = os.path.dirname(test_path)
-scripts_path = os.path.join(modules_path, "acl_loader")
 
 class TestShowMirror(object):
     @classmethod
     def setup_class(cls):
         print("SETUP")
-        os.environ["PATH"] += os.pathsep + scripts_path
-        print(os.pathsep + scripts_path)
         os.environ["UTILITIES_UNIT_TESTING"] = "1"
 
     def test_mirror_show(self):
         from .mock_tables import dbconnector
         jsonfile_config = os.path.join(mock_db_path, "config_db")
         dbconnector.dedicated_dbs['CONFIG_DB'] = jsonfile_config
+        runner = CliRunner()
+        db = Db()
         expected_output = """\
 ERSPAN Sessions
 Name    Status    SRC IP    DST IP    GRE    DSCP    TTL    Queue    Policer    Monitor Port    SRC Port    Direction
@@ -39,11 +36,8 @@ session2   active    Ethernet7   Ethernet8   both
 session11  active    Ethernet9   Ethernet10  rx
 session15  active    Ethernet2   Ethernet3   tx
 """
-
-        cmd = 'python ' + os.path.join(scripts_path, 'main.py') + ' show session'
-        return_code, result = get_result_and_return_code(cmd)
-        print("return_code: {}".format(return_code))
-        print("result = {}".format(result))
+        aclloader = AclLoader()
+        result = runner.invoke(acl_loader_show.cli.commands['show'].commands['session'], [], obj=db)
         dbconnector.dedicated_dbs = {}
         assert return_code == 0
         assert result == expected_output
