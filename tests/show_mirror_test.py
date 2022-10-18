3448b7b68128cf3e39078a7036bcef27534f30f1
pettershao-ragilenetworks <81281940+pettershao-ragilenetworks@users.noreply.github.com>
2022-10-18 13:46:02 +0800

    Update show_mirror_test.py

--- tests/show_mirror_test.py
+++ tests/show_mirror_test.py
@@ -5,26 +5,18 @@ from swsscommon.swsscommon import SonicV2Connector
 from click.testing import CliRunner
 from utilities_common.db import Db
 
-import mock_tables.dbconnector
 import acl_loader.main as acl_loader_show
 from acl_loader import *
 from acl_loader.main import *
 
-test_path = os.path.dirname(os.path.abspath(__file__))
-mock_db_path = os.path.join(test_path, "mirror_input")
-
 class TestShowMirror(object):
-    @classmethod
-    def setup_class(cls):
-        print("SETUP")
-        os.environ["UTILITIES_UNIT_TESTING"] = "1"
-
     def test_mirror_show(self):
-        from .mock_tables import dbconnector
-        jsonfile_config = os.path.join(mock_db_path, "config_db")
-        dbconnector.dedicated_dbs['CONFIG_DB'] = jsonfile_config
         runner = CliRunner()
         aclloader = AclLoader()
+        aclloader.configdb.set_entry("MIRROR_SESSION", "session1", {"direction": "BOTH", "dst_port": "Ethernet30", "src_port": "Ethernet40", "type": "SPAN"})
+        aclloader.configdb.set_entry("MIRROR_SESSION", "session2", {"direction": "BOTH", "dst_port": "Ethernet7", "src_port": "Ethernet8", "type": "SPAN"})
+        aclloader.configdb.set_entry("MIRROR_SESSION", "session11", {"direction": "RX", "dst_port": "Ethernet9", "src_port": "Ethernet10", "type": "SPAN"})
+        aclloader.configdb.set_entry("MIRROR_SESSION", "session15", {"direction": "TX", "dst_port": "Ethernet2", "src_port": "Ethernet3", "type": "SPAN"})
         context = {
             "acl_loader": aclloader
         }
@@ -42,6 +34,5 @@ session11  active    Ethernet9   Ethernet10  rx
 session15  active    Ethernet2   Ethernet3   tx
 """
         result = runner.invoke(acl_loader_show.cli.commands['show'].commands['session'], [], obj=context)
-        dbconnector.dedicated_dbs = {}
         assert result.exit_code == 0
         assert result.output == expected_output
