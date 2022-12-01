74e86745487665666dd83f8a1e6546a90445f00f
wenyiz2021 <91497961+wenyiz2021@users.noreply.github.com>
2022-11-30 17:06:02 -0800

    Update show_run_bgp_test.py

--- tests/show_run_bgp_test.py
+++ tests/show_run_bgp_test.py
@@ -106,7 +106,7 @@ class TestShowRunCommands(object):
     def test_show_run_bgp_single(self):
         runner = CliRunner()
         exec_cmd = show.cli.commands["runningconfiguration"].commands["bgp"]
-        result = runner.invoke(exec_cmd, input['args'])
+        result = runner.invoke(exec_cmd)
         print("{}".format(result.output))
         assert result.exit_code == 0
         assert result.output == show_run_bgp_sasic
