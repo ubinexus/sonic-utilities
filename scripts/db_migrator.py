94111966d0ce73153e454fc5e4fc884dd005dbdf
RoRonoa <umarasad20@gmail.com>
2023-02-25 03:51:01 +0500

    updated db_migrator tests

--- scripts/db_migrator.py
+++ scripts/db_migrator.py
@@ -469,6 +469,9 @@ class DBMigrator():
                 if pc_key in vlan_member_keys:
                     pc_value["mode"] = "trunk"
                     self.configDB.set_entry("PORTCHANNEL", pc_key, pc_value)
+                else:
+                    pc_value["mode"] = "routed"
+                    self.configDB.set_entry("PORTCHANNEL", pc_key, pc_value)
 
 
     def migrate_qos_db_fieldval_reference_remove(self, table_list, db, db_num, db_delimeter):
