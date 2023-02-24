ae002972832c2a4837b0667f74df3dc74630869c
RoRonoa <umarasad20@gmail.com>
2023-02-25 00:30:35 +0500

    db_migrator switchport test update

--- tests/db_migrator_test.py
+++ tests/db_migrator_test.py
@@ -265,12 +265,6 @@ class TestSwitchPortMigrator(object):
         dbconnector.dedicated_dbs['CONFIG_DB'] = os.path.join(mock_db_path, 'config_db', 'switchport-expected')
         expected_db = Db()
         advance_version_for_expected_database(dbmgtr.configDB, expected_db.cfgdb, 'version_4_0_1')
-        test = str(dbmgtr.configDB.get_table('PORT'))
-        test += str(expected_db.cfgdb.get_table('PORT'))
-        test += str(expected_db.cfgdb.get_table('PORTCHANNEL'))
-        test += str(expected_db.cfgdb.get_table('PORTCHANNEL'))
-
-        assert test == "1"
 
         assert dbmgtr.configDB.get_table('PORT') == expected_db.cfgdb.get_table('PORT')
         assert dbmgtr.configDB.get_table('PORTCHANNEL') == expected_db.cfgdb.get_table('PORTCHANNEL')
