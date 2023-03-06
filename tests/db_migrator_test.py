ba9c2a517c3c45edfaa8dc8d69645ba33794b5f2
afeigin <afeigin@nvidia.com>
2023-03-06 08:42:50 +0000

    fix fast-reboot db-migrator test

--- tests/db_migrator_test.py
+++ tests/db_migrator_test.py
@@ -467,17 +467,18 @@ class TestFastRebootTableModification(object):
     def test_rename_fast_reboot_table_check_enable(self):
         device_info.get_sonic_version_info = get_sonic_version_info_mlnx
         dbconnector.dedicated_dbs['STATE_DB'] = os.path.join(mock_db_path, 'state_db', 'fast_reboot_input')
+        dbconnector.dedicated_dbs['CONFIG_DB'] = os.path.join(mock_db_path, 'config_db', 'empty-config-input')
         
         import db_migrator
         dbmgtr = db_migrator.DBMigrator(None)
         dbmgtr.migrate()
         
         dbconnector.dedicated_dbs['STATE_DB'] = os.path.join(mock_db_path, 'state_db', 'fast_reboot_expected')
+        expected_db = SonicV2Connector(host='127.0.0.1')
+        expected_db.connect(expected_db.STATE_DB)
 
-        expected_db = Db()
-
-        resulting_table = dbmgtr.stateDB.get_table('FAST_RESTART_ENABLE_TABLE')
-        expected_table = expected_db.stateDB.get_table('FAST_RESTART_ENABLE_TABLE')
+        resulting_table = dbmgtr.stateDB.get_all(dbmgtr.stateDB.STATE_DB, 'FAST_RESTART_ENABLE_TABLE|system')
+        expected_table = expected_db.get_all(expected_db.STATE_DB, 'FAST_RESTART_ENABLE_TABLE|system')
 
         diff = DeepDiff(resulting_table, expected_table, ignore_order=True)
         assert not diff
