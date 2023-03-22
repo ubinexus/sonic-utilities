a95484f6ab496f21b5a805680818f3327ce07682
Stepan Blyschak <stepanb@nvidia.com>
2023-03-22 19:15:51 +0200

    add UT for uninstall
    Signed-off-by: Stepan Blyschak <stepanb@nvidia.com>

--- tests/sonic_package_manager/test_manager.py
+++ tests/sonic_package_manager/test_manager.py
@@ -168,16 +168,28 @@ def test_installation_cli_plugin(package_manager, fake_metadata_resolver, anythi
         package_manager.docker.extract.assert_called_once_with(anything, '/cli/plugin.py', '/test-package_0.py')
 
 
-def test_installation_multiple_cli_plugin(package_manager, fake_metadata_resolver, anything):
+def test_installation_multiple_cli_plugin(package_manager, fake_metadata_resolver, mock_feature_registry, anything):
     manifest = fake_metadata_resolver.metadata_store['Azure/docker-test']['1.6.0']['manifest']
     manifest['cli']= {'show': ['/cli/plugin.py', '/cli/plugin2.py']}
-    with patch('sonic_package_manager.manager.get_cli_plugin_directory') as get_dir_mock:
+    with patch('sonic_package_manager.manager.get_cli_plugin_directory') as get_dir_mock, \
+         patch('os.remove') as remove_mock, \
+         patch('os.path.exists') as path_exists_mock:
         get_dir_mock.return_value = '/'
         package_manager.install('test-package')
         package_manager.docker.extract.assert_has_calls(
             [
                 call(anything, '/cli/plugin.py', '/test-package_0.py'),
                 call(anything, '/cli/plugin2.py', '/test-package_1.py'),
+            ],
+            any_order=True,
+        )
+
+        package_manager._set_feature_state = Mock()
+        package_manager.uninstall('test-package', force=True)
+        remove_mock.assert_has_calls(
+            [
+                call('/test-package_0.py'),
+                call('/test-package_1.py'),
             ],
             any_order=True,
         )
