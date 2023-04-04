4e9b5616f94259798307788ad4fca56afbe56e55
Isabella de Leon <ideleon@microsoft.com>
2023-04-04 14:57:27 -0700

    [show/tests] Add support for "show run isis"
    Signed-off-by: ideleon@microsoft.com

--- tests/isis_frr_input/isis_frr_test_vector.py
+++ tests/isis_frr_input/isis_frr_test_vector.py
@@ -452,6 +452,53 @@ def mock_show_isis_interface(request):
     else:
         return ""
 
+show_run_isis_output = \
+"""Building configuration...
+
+Current configuration:
+!
+frr version 8.2.2
+frr defaults traditional
+hostname vlab-01
+log syslog informational
+log facility local4
+no service integrated-vtysh-config
+!
+password zebra
+enable password zebra
+!
+interface PortChannel101
+ ip router isis 1
+ ipv6 router isis 1
+ isis network point-to-point
+exit
+!
+router isis 1
+ is-type level-2-only
+ net 49.0001.1720.1700.0002.00
+ lsp-mtu 1383
+ lsp-timers level-1 gen-interval 30 refresh-interval 900 max-lifetime 1200
+ lsp-timers level-2 gen-interval 30 refresh-interval 305 max-lifetime 900
+ log-adjacency-changes
+exit
+!
+end
+"""
+
+show_run_isis_invalid_help_output = \
+"""Usage: isis [OPTIONS]
+Try "isis --help" for help.
+
+Error: Got unexpected extra argument (?)
+"""
+
+def mock_show_run_isis(request):
+    if request.param == 'show_run_isis_output':
+        return show_run_isis_output
+    elif request.param == 'show_run_isis_invalid_help_output':
+        return show_run_isis_invalid_help_output
+    else:
+        return ""
 
 testData = {
     'isis_neighbors': {
@@ -553,5 +600,15 @@ testData = {
         'args': ['sonic1'],
         'rc': 2,
         'rc_output': isis_interface_unknown_ifname_output
+    },
+    'show_run_isis': {
+        'args': [],
+        'rc': 0,
+        'rc_output': show_run_isis_output
+    },
+    'show_run_isis_invalid_help': {
+        'args': ['?'],
+        'rc': 2,
+        'rc_output': show_run_isis_invalid_help_output
     },
 }
