82f3f7ef889966fb12f1b93c9835b6172349d471
Mai Bui <maibui@microsoft.com>
2023-04-06 04:41:29 +0000

    add UT
    Signed-off-by: Mai Bui <maibui@microsoft.com>

--- tests/sonic_package_manager/test_service_creator.py
+++ tests/sonic_package_manager/test_service_creator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import sys
 import copy
 from unittest.mock import Mock, MagicMock, call, patch
 
@@ -71,6 +72,13 @@ def test_is_list_of_strings():
 
     output = is_list_of_strings(['a', 'b', 1])
     assert output is False
+
+def test_run_command():
+    with pytest.raises(SystemExit) as e:
+        run_command('echo 1')
+
+    with pytest.raises(ServiceCreatorError) as e:
+        run_command([sys.executable, "-c", "import sys; sys.exit(6)"])
 
 @pytest.fixture()
 def service_creator(mock_feature_registry,
