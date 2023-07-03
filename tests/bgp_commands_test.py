581e585206a03f74f1e3f458c43fb4c853e7feb4
devojha <47282568+developfast@users.noreply.github.com>
2023-07-03 18:27:49 +0000

    corrected multi asic setup for crm test

--- tests/bgp_commands_test.py
+++ tests/bgp_commands_test.py
@@ -255,7 +255,6 @@ class TestBgpCommandsMultiAsic(object):
     def setup_class(cls):
         print("SETUP")
         os.environ["UTILITIES_UNIT_TESTING"] = "2"
-        os.environ["UTILITIES_UNIT_TESTING_TOPOLOGY"] = "multi_asic"
         from .mock_tables import mock_multi_asic
         importlib.reload(mock_multi_asic)
         from .mock_tables import dbconnector
@@ -293,9 +292,7 @@ class TestBgpCommandsMultiAsic(object):
     def teardown_class(cls):
         print("TEARDOWN")
         os.environ["UTILITIES_UNIT_TESTING"] = "0"
-        os.environ["UTILITIES_UNIT_TESTING_TOPOLOGY"] = ""
         from .mock_tables import mock_single_asic
         importlib.reload(mock_single_asic)
         from .mock_tables import dbconnector
         dbconnector.load_database_config()
-        dbconnector.load_namespace_config()
