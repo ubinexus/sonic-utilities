20005e95c8a2f50bc730d0496d3875efe11de3b0
Anoop Kamath <115578705+AnoopKamath@users.noreply.github.com>
2023-09-28 17:20:27 -0700

    Remove duplicate code

--- sfputil/main.py
+++ sfputil/main.py
@@ -1520,33 +1520,6 @@ def unlock(port_name, password):
     else:
         click.echo("CDB: Host password NOT accepted! status = {}".format(status))
 
-# 'target' subcommand
-@firmware.command()
-@click.argument('port_name', required=True, default=None)
-@click.argument('target', type=click.INT, required=True, default=None)
-def target(port_name, target):
-    """Select target end for firmware download 0(local), 1(remote-A), 2-(remote-B)"""
-    physical_port = logical_port_to_physical_port_index(port_name)
-    sfp = platform_chassis.get_sfp(physical_port)
-
-    if is_port_type_rj45(port_name):
-        click.echo("This functionality is not applicable for RJ45 port {}.".format(port_name))
-        sys.exit(EXIT_FAIL)
-
-    if not is_sfp_present(port_name):
-       click.echo("{}: SFP EEPROM not detected\n".format(port_name))
-       sys.exit(EXIT_FAIL)
-
-    try:
-        api = sfp.get_xcvr_api()
-    except NotImplementedError:
-        click.echo("This functionality is currently not implemented for this platform")
-        sys.exit(ERROR_NOT_IMPLEMENTED)
-
-    status = api.set_firmware_download_target_end(target)
-    click.echo("{}". format(status))
-    sys.exit(EXIT_FAIL)
-    
 # 'version' subcommand
 @cli.command()
 def version():
