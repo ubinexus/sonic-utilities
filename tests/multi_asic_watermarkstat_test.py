40476a8ff0f493405549547f846f849c66226a33
bktsim <bktsim@arista.com>
2023-11-30 11:20:35 -0800

    add test for invalid namespace

--- tests/multi_asic_watermarkstat_test.py
+++ tests/multi_asic_watermarkstat_test.py
@@ -19,20 +19,25 @@ class TestWatermarkstatMultiAsic(object):
       os.environ["UTILITIES_UNIT_TESTING_TOPOLOGY"] = "multi_asic"
       print("SETUP")
 
-   def execute(self, command, expected_result):
+   def execute(self, command, expected_code=0, expected_result=None):
       return_code, result = get_result_and_return_code(command)
       print("return_code: {}".format(return_code))
       print("result = {}".format(result))
-      assert return_code == 0
-      assert result == expected_result
+      assert return_code == expected_code
+      if expected_result:
+         assert result == expected_result
 
    def test_show_pg_shared_masic(self):
       self.execute(['watermarkstat', '-t', 'pg_shared', '-n', 'asic1'],
-                   show_pg_wm_shared_output_masic)
+                   expected_result=show_pg_wm_shared_output_masic)
 
    def test_show_headroom_pool_masic(self):
       self.execute(['watermarkstat', '-t', 'headroom_pool', '-n', 'asic1'],
-                   show_hdrm_pool_wm_output)
+                   expected_result=show_hdrm_pool_wm_output)
+
+   def test_show_invalid_asic_masic(self):
+      self.execute(['watermarkstat', '-t', 'headroom_pool', '-n', 'asic15'],
+                   expected_code=1)
 
    @classmethod
    def teardown_class(cls):
