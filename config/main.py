bd50175cda9edc6d089d9416993383d095bf9140
Anoop Kamath <115578705+AnoopKamath@users.noreply.github.com>
2023-12-05 15:25:29 -0800

    Address review comments
    Remove STR casting and if check for more than 1 config_db json files

--- config/main.py
+++ config/main.py
@@ -1518,14 +1518,13 @@ def reload(db, filename, yes, load_sysinfo, no_service_restart, force, file_form
     #Validate config_db.json file
     if file_format == 'config_db':
         if os.path.exists(DEFAULT_CONFIG_DB_FILE):
-            command = [str(SONIC_CFGGEN_PATH), '-j', str(DEFAULT_CONFIG_DB_FILE)]
-            clicommon.run_command(command, display_cmd=True)
-        if  num_cfg_file > 1:
-            for inst in range(0, num_cfg_file-1):
-                file = "/etc/sonic/config_db{}.json".format(inst)
-                if os.path.exists(file) :
-                    command = [str(SONIC_CFGGEN_PATH), '-j', str(file)]
-                    clicommon.run_command(command, display_cmd=True)
+            command = [SONIC_CFGGEN_PATH, '-j', DEFAULT_CONFIG_DB_FILE]
+            clicommon.run_command(command)
+        for inst in range(0, num_cfg_file-1):
+            file = "/etc/sonic/config_db{}.json".format(inst)
+            if os.path.exists(file):
+                command = [SONIC_CFGGEN_PATH, '-j', file]
+                clicommon.run_command(command)
     
     #Stop services before config push
     if not no_service_restart:
