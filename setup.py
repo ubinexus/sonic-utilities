0991d6d1d85310415511bb3e7355630cefa7e769
shiraez <shiraez@marvell.com>
2024-02-21 09:34:56 +0200

    add coldreboot script to setup

--- setup.py
+++ setup.py
@@ -115,6 +115,7 @@ setup(
         'scripts/asic_config_check',
         'scripts/boot_part',
         'scripts/buffershow',
+        'scripts/coldreboot',
         'scripts/coredump-compress',
         'scripts/configlet',
         'scripts/db_migrator.py',
