3aec0f93d64a6265b095b8208841231e7048fa3e
Xincun Li <xincun.li@microsoft.com>
2024-04-30 18:39:45 -0700

    fix pre commit test

--- generic_config_updater/generic_updater.py
+++ generic_config_updater/generic_updater.py
@@ -84,13 +84,13 @@ class PatchApplier:
         self.config_wrapper.validate_field_operation(old_config, target_config)
 
         # Validate target config does not have empty tables since they do not show up in ConfigDb
-        self.logger.log_notice(f"{scope}: validating target config does not have empty tables, 
+        self.logger.log_notice(f"{scope}: validating target config does not have empty tables, \
                                since they do not show up in ConfigDb.")
         empty_tables = self.config_wrapper.get_empty_tables(target_config)
         if empty_tables: # if there are empty tables
             empty_tables_txt = ", ".join(empty_tables)
-            raise EmptyTableError(f"{scope}: given patch is not valid because it will result in empty tables
-                                  which is not allowed in ConfigDb.
+            raise EmptyTableError(f"{scope}: given patch is not valid because it will result in empty tables \
+                                  which is not allowed in ConfigDb. \
                                   Table{'s' if len(empty_tables) != 1 else ''}: {empty_tables_txt}")
 
         # Generate list of changes to apply
