9cb79de661482d63f3f0e057b1137c5bcc64093e
Bilal Ismail <110652537+bilal-ismail@users.noreply.github.com>
2024-05-06 10:27:35 +0000

    changes so far for the kdump new attributes for remote ssh usecase

--- config/kdump.py
+++ config/kdump.py
@@ -79,7 +79,7 @@ def kdump_memory(db, kdump_memory):
 
 
 #
-# 'num_dumps' command ('sudo config keump num_dumps ...')
+# 'num_dumps' command ('sudo config kdump num_dumps ...')
 #
 @kdump.command(name="num_dumps", short_help="Configure the maximum dump files of KDUMP mechanism")
 @click.argument('kdump_num_dumps', metavar='<kdump_num_dumps>', required=True, type=int)
@@ -90,3 +90,39 @@ def kdump_num_dumps(db, kdump_num_dumps):
     check_kdump_table_existence(kdump_table)
 
     db.cfgdb.mod_entry("KDUMP", "config", {"num_dumps": kdump_num_dumps})
+
+
+#
+# 'num_dumps' command ('sudo config kdump remote ssh -c ... -k ...')
+# 'num_dumps' command ('sudo config kdump remote disable ...')
+#
+@kdump.command(name="remote", short_help="Configure remote KDUMP mechanism")
+@click.argument("action", type=click.Choice(["ssh", "disable"]))
+@click.option("-c", "--connection-string", help="SSH user and host", required=false)
+@click.option("-k", "--private-key", help="Path to private key", default="/root/.ssh/kdump_id_rsa", required=false)
+@pass_db
+def kdump_remote(action, connection_string, private_key):
+    """Configure remote KDUMP mechanism"""
+    kdump_table = db.cfgdb.get_table("KDUMP")
+    check_kdump_table_existence(kdump_table)
+
+    
+    if action == "ssh":
+        if connection_string is not None:
+            db.cfgdb.mod_entry("KDUMP", "config", {"connection_string": connection_string})
+
+        if private_key is not None:
+            db.cfgdb.mod_entry("KDUMP", "config", {"private_key": private_key})
+
+        if connection_string is None or private_key is None:
+            click.echo("Error: Both --connection_string and --private-key are required for SSH configuration.")
+            sys.exit(1)
+        # Enable (uncomment) SSH comand in config file
+        pass
+    elif action == "disable":
+        # Execute disable command
+        pass
+
+    click.echo("KDUMP configuration changes may require a reboot to take effect.")    
+    click.echo("Save SONiC configuration using 'config save' before issuing the reboot command.")
+
