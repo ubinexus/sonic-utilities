8afda8e4dd68c9f9f9f4146481745808cf510481
Asad Raza <araza07@gmail.com>
2024-05-09 11:57:34 +0500

    Add test case

--- tests/dhcp_rate_test.py
+++ tests/dhcp_rate_test.py
@@ -4,8 +4,47 @@ from unittest import mock
 from click.testing import CliRunner
 
 import config.main as config
+import show.main as show
 from utilities_common.db import Db
 import utilities_common.bgp_util as bgp_util
+
+
+test_config_dhcp_rate_add_del_output = """\
+Interface      DHCP Mitigation Rate
+-----------  ----------------------
+Ethernet0                       300
+Ethernet4                       300
+Ethernet8                       300
+Ethernet12                      300
+Ethernet16                      300
+Ethernet20                      300
+Ethernet24
+Ethernet28                      300
+Ethernet32                       45
+Ethernet36                      300
+Ethernet40                      300
+Ethernet44                      300
+Ethernet48                      300
+Ethernet52                      300
+Ethernet56                      300
+Ethernet60                      300
+Ethernet64                      300
+Ethernet68                      300
+Ethernet72                      300
+Ethernet76                      300
+Ethernet80                      300
+Ethernet84                      300
+Ethernet88                      300
+Ethernet92                      300
+Ethernet96                      300
+Ethernet100                     300
+Ethernet104                     300
+Ethernet108                     300
+Ethernet112                     300
+Ethernet116                     300
+Ethernet120                     300
+Ethernet124                     300
+"""
 
 
 class TestDHCPRate(object):
@@ -113,6 +152,39 @@ class TestDHCPRate(object):
         print(result.output)
         assert result.exit_code != 0
         assert "Error: 20 DHCP rate limit does not exist on Ethernet0." in result.output
+
+    def test_config_dhcp_rate_add_del(self):
+        db = Db()
+        runner = CliRunner()
+        obj = {'config_db': db.cfgdb}
+
+        # Remove default DHCP rate limit from Ethernet24
+        result = runner.invoke(config.config.commands["interface"].commands["dhcp-mitigation-rate"].commands["del"],
+                               ["Ethernet24", "300"], obj=obj)
+        print(result.exit_code)
+        print(result.output)
+        assert result.exit_code == 0
+
+        # Remove default DHCP rate limit from Ethernet32
+        result = runner.invoke(config.config.commands["interface"].commands["dhcp-mitigation-rate"].commands["del"],
+                               ["Ethernet32", "300"], obj=obj)
+        print(result.exit_code)
+        print(result.output)
+        assert result.exit_code == 0
+
+        # Add DHCP rate limit 45 on Ethernet32
+        result = runner.invoke(config.config.commands["interface"].commands["dhcp-mitigation-rate"].commands["add"],
+                               ["Ethernet32", "45"], obj=obj)
+        print(result.exit_code)
+        print(result.output)
+        assert result.exit_code == 0
+
+        # show output
+        result = runner.invoke(show.cli.commands["interfaces"].commands["dhcp-mitigation-rate"], [], obj=obj)
+        print(result.exit_code)
+        print(result.output)
+        assert result.exit_code == 0
+        assert result.output == test_config_dhcp_rate_add_del_output
 
     @classmethod
     def teardown_class(cls):
