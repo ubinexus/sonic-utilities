7d478dce507872dbc689f271e2f11ca71a003561
Ashwin Srinivasan <assrinivasan@microsoft.com>
2024-05-31 20:31:50 +0000

    Flake8 test fixes

--- tests/ssdutil_test.py
+++ tests/ssdutil_test.py
@@ -6,13 +6,13 @@ sys.modules['argparse'] = MagicMock()
 
 import ssdutil.main as ssdutil  # noqa: E402
 
-# Add mocked_libs path so that the file under test can load mocked modules from there
+# Add mocked_libs path so that the file can load mocked modules from there
 tests_path = os.path.dirname(os.path.abspath(__file__))
 mocked_libs_path = os.path.join(tests_path, "mocked_libs")
 sys.path.insert(0, mocked_libs_path)
 
-from .mocked_libs import sonic_platform_base #.sonic_platform_base.sonic_ssd import ssd_generic
-#from .mocked_libs  #.sonic_platform_base.sonic_storage import ssd
+from .mocked_libs import sonic_platform_base  # noqa: E402,F401
+
 
 test_path = os.path.dirname(os.path.abspath(__file__))
 modules_path = os.path.dirname(test_path)
@@ -59,8 +59,8 @@ class TestSsdutil:
     @patch('sonic_py_common.device_info.get_paths_to_platform_and_hwsku_dirs',
            MagicMock(return_value=("test_path", "")))
     @patch('os.geteuid', MagicMock(return_value=0))
-    @patch('sonic_platform_base.sonic_ssd.ssd_generic.SsdUtil', MagicMock(return_value=Ssd()))
-    @patch('sonic_platform_base.sonic_storage.ssd.SsdUtil', MagicMock(return_value=Ssd()))
+    @patch('sonic_platform_base.sonic_ssd.ssd_generic.SsdUtil', MagicMock(return_value=Ssd()))  # noqa: E501
+    @patch('sonic_platform_base.sonic_storage.ssd.SsdUtil', MagicMock(return_value=Ssd()))  # noqa: E501
     def test_happy_path(self):
 
         ssdutil.ssdutil()
