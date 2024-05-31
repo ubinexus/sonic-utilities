c56718f85ec9f3b12274204b4f3b08a3e17da22c
Ashwin Srinivasan <assrinivasan@microsoft.com>
2024-05-31 20:28:09 +0000

    Made ssdutil UT backwards compatible

---
+++ tests/mocked_libs/sonic_platform_base/sonic_storage/ssd.py
@@ -0,0 +1,20 @@
+
+class SsdUtil():
+
+    def get_model(self):
+        return 'SkyNet'
+
+    def get_firmware(self):
+        return 'ABC'
+
+    def get_serial(self):
+        return 'T1000'
+
+    def get_health(self):
+        return 5
+
+    def get_temperature(self):
+        return 3000
+
+    def get_vendor_output(self):
+        return 'SONiC Test'
