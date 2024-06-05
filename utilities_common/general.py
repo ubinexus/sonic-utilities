c28c1300e64caadd299d08e217341d939b43f061
mlok <marty.lok@nokia.com>
2024-06-05 16:58:32 -0400

    [mutli-asic][lldp] "show lldp table" displays error message in output on multi-asic linecard

--- utilities_common/general.py
+++ utilities_common/general.py
@@ -4,6 +4,9 @@ import sys
 
 from sonic_py_common.multi_asic import is_multi_asic
 from swsscommon import swsscommon
+FEATURE_TABLE = "FEATURE"
+FEATURE_HAS_PER_ASIC_SCOPE = 'has_per_asic_scope'
+FEATURE_HAS_GLOBAL_SCOPE = 'has_global_scope'
 
 def load_module_from_source(module_name, file_path):
     """
@@ -42,3 +45,29 @@ def get_optional_value_for_key_in_config_tbl(config_db, port, key, table):
         
     return value
 
+def get_feature_state_data(config_db, feature):
+    '''
+    Get feature state from FEATURE table from CONFIG_DB. 
+    return  global_scope, per_asic_scope 
+    - if feature state is disabled, return "False" for both global_scope and per_asic_scope
+    - if is not a multi-asic, return feature state for global_scope ("True/False") and
+      "False" for asic_scope
+    '''
+    
+    global_scope = ""
+    asic_scope = ""
+    info_dict = {}
+    info_dict = config_db.get_entry(FEATURE_TABLE,feature)
+    if info_dict is None:
+        return global_scope, asic_scope 
+    if is_multi_asic():
+        if info_dict['state'].lower() == "enabled":
+            global_scope = info_dict[FEATURE_HAS_GLOBAL_SCOPE]
+            asic_scope = info_dict[FEATURE_HAS_PER_ASIC_SCOPE]
+        else:
+            global_scope = "False"
+            asic_scope = "False"
+    else:
+        if info_dict['state'].lower() == "enabled":
+            global_scope = "True"
+    return global_scope, asic_scope 
