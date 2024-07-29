ebaefc0ac7fca4bcd6fbdfbea89f1e1180b5b7d1
muhammadalihussnain <ali_hussnain@live.com>
2024-07-29 13:14:08 +0500

    typo

--- config/kdump.py
+++ config/kdump.py
@@ -97,7 +97,7 @@ def kdump_num_dumps(db, kdump_num_dumps):
     db.cfgdb.mod_entry("KDUMP", "config", {"num_dumps": kdump_num_dumps})
     echo_reboot_warning()
 # 'remote' command ('sudo config kdump remote ...')
-    
+
 
 @kdump.command(name="remote", short_help="Enable or Disable Kdump Remote")
 @click.argument('action', required=True, type=click.Choice(['enable', 'disable'], case_sensitive=False))
@@ -215,7 +215,7 @@ def add_kdump_item(db, item, value):
     # except Exception as e:
     #     click.echo(f"Error updating /etc/default/kdump-tools: {e}")
     # echo_reboot_warning(
-    
+
 
 @kdump.command(name="remove", short_help="Remove SSH connection string or SSH key path.")
 @click.argument('item', type=click.Choice(['ssh_string', 'ssh_path']))
