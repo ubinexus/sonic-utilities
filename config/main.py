8da7657f434261f3a3f00bd0e2e647435ddd43bc
irene_pan <irene_pan@edge-core.com>
2024-07-30 08:38:49 +0000

    [NTP] When mgmtvrf is enabled, the NTP vrf should be changed to "mgmt"
    Signed-off-by: irene_pan irene_pan@edge-core.com

--- config/main.py
+++ config/main.py
@@ -3460,6 +3460,7 @@ def vrf_add_management_vrf(config_db):
         return None
     try:
         config_db.mod_entry('MGMT_VRF_CONFIG', "vrf_global", {"mgmtVrfEnabled": "true"})
+        config_db.mod_entry('NTP', "global", {"vrf": "mgmt"})
     except ValueError as e:
         ctx = click.get_current_context()
         ctx.fail("Invalid ConfigDB. Error: {}".format(e))
@@ -3474,6 +3475,7 @@ def vrf_delete_management_vrf(config_db):
         return None
     try:
         config_db.mod_entry('MGMT_VRF_CONFIG', "vrf_global", {"mgmtVrfEnabled": "false"})
+        config_db.mod_entry('NTP', "global", {"vrf": "default"})
     except ValueError as e:
         ctx = click.get_current_context()
         ctx.fail("Invalid ConfigDB. Error: {}".format(e))
