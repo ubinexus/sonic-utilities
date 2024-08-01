7f82d67ffa90987474f5ad9b2f2dcf02c7cd6f71
Mai Bui <maibui@microsoft.com>
2024-08-01 16:37:52 +0000

    show auditd CLI cont
    Signed-off-by: Mai Bui <maibui@microsoft.com>

---
+++ show/audit.py
@@ -0,0 +1,12 @@
+import click
+import utilities_common.cli as clicommon
+
+#
+# 'audit' group ("show audit")
+#
+
+@click.group(cls=clicommon.AliasedGroup, invoke_without_command=True)
+def audit():
+    """Show details of the audit enhancement"""
+    cmd = ['sudo', 'auditctl', '-l']
+    clicommon.run_command(cmd)
