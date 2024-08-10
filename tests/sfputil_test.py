0de47b2a87724d289f287759b9bfc8c63a419718
xinyu <xinyu0123@gmail.com>
2024-08-10 10:22:26 +0800

    [sfputil] Configure the debug loopback mode only on the relevant lanes of the logical port
    Signed-off-by: xinyu <xinyu0123@gmail.com>

--- tests/sfputil_test.py
+++ tests/sfputil_test.py
@@ -1633,6 +1633,9 @@ EEPROM hexdump for port Ethernet4
     @patch('sfputil.main.platform_chassis')
     @patch('sfputil.main.platform_sfputil', MagicMock(is_logical_port=MagicMock(return_value=1)))
     @patch('sfputil.main.logical_port_to_physical_port_index', MagicMock(return_value=1))
+    @patch('sonic_py_common.multi_asic.get_front_end_namespaces', MagicMock(return_value=['']))
+    @patch('sfputil.main.ConfigDBConnector', MagicMock())
+    @patch('sfputil.main.SonicV2Connector', MagicMock())
     def test_debug_loopback(self, mock_chassis):
         mock_sfp = MagicMock()
         mock_api = MagicMock()
@@ -1659,6 +1662,12 @@ EEPROM hexdump for port Ethernet4
         assert result.output == 'Ethernet0: Set host-side-input loopback\n'
         assert result.exit_code != ERROR_NOT_IMPLEMENTED
 
+        mock_sfp.get_xcvr_api = MagicMock(return_value=mock_api)
+        result = runner.invoke(sfputil.cli.commands['debug'].commands['loopback'],
+                               ["Ethernet0", "media-side-input"])
+        assert result.output == 'Ethernet0: Set media-side-input loopback\n'
+        assert result.exit_code != ERROR_NOT_IMPLEMENTED
+
         mock_api.set_loopback_mode.return_value = False
         result = runner.invoke(sfputil.cli.commands['debug'].commands['loopback'],
                                ["Ethernet0", "none"])
@@ -1671,3 +1680,8 @@ EEPROM hexdump for port Ethernet4
                                ["Ethernet0", "none"])
         assert result.output == 'Ethernet0: Set loopback mode is not applicable for this module\n'
         assert result.exit_code == ERROR_NOT_IMPLEMENTED
+
+        mock_api.set_loopback_mode.side_effect = [TypeError, True]
+        result = runner.invoke(sfputil.cli.commands['debug'].commands['loopback'],
+                               ["Ethernet0", "none"])
+        assert result.output == 'Ethernet0: Set none loopback\n'
