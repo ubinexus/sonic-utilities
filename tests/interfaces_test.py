0148cda83d9a67bb4f23db9367ce2d5430a85645
Muhammad Ali Hussnain <91005947+muhammadalihussnain@users.noreply.github.com>
2024-09-09 11:13:42 +0000

    testing variable 266 intfcs_test.py

--- tests/interfaces_test.py
+++ tests/interfaces_test.py
@@ -263,13 +263,13 @@ PortChannel0004  routed
 PortChannel1001  trunk               4000
 """
 
-show_dhcp_rate_limit_in_alias_mode_output = """\
+'''show_dhcp_rate_limit_in_alias_mode_output = """\
 Interface      DHCP Mitigation Rate
 -----------  ----------------------
 etp1                            300
 etp2
 """
-
+'''
 
 class TestInterfaces(object):
     @classmethod
