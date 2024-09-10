1a79116283597d0104e3c9bb6f2df511b1273010
Vaibhav Dahiya <vdahiya@microsoft.com>
2024-09-10 00:26:23 +0000

    pep 8
    Signed-off-by: Vaibhav Dahiya <vdahiya@microsoft.com>

--- show/interfaces/__init__.py
+++ show/interfaces/__init__.py
@@ -680,8 +680,11 @@ def fetch_fec_histogram(port_oid_map, target_port):
     asic_db_kvp = counter_db.get_all(counter_db.COUNTERS_DB, 'COUNTERS:{}'.format(port_oid))
 
     if asic_db_kvp is not None:
-        fec_errors = {f'BIN{i}': asic_db_kvp.get
-                         (f'SAI_PORT_STAT_IF_IN_FEC_CODEWORD_ERRORS_S{i}', '0') for i in range(16)}
+
+        fec_errors = {
+            f'BIN{i}': asic_db_kvp.get(f'SAI_PORT_STAT_IF_IN_FEC_CODEWORD_ERRORS_S{i}', '0')
+            for i in range(16)
+        }
 
         # Prepare the data for tabulation
         table_data = [(bin_label, error_value) for bin_label, error_value in fec_errors.items()]
