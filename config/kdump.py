3951942148545879c15c15c72b00de2cc5aa9045
Muhammad Ali Hussnain <91005947+muhammadalihussnain@users.noreply.github.com>
2024-09-24 10:14:47 +0000

    testcases typos

--- config/kdump.py
+++ config/kdump.py
@@ -96,10 +96,10 @@ def kdump_num_dumps(db, kdump_num_dumps):
     echo_reboot_warning()
 
 
-@kdump.command(name ="remote", help="Configure the remote enable/disable for KDUMP mechanism")
+@kdump.command(name="remote", help="Configure the remote enable/disable for KDUMP mechanism")
 @click.argument('action', metavar='<enable/disable>', required=True, type=click.STRING)  # Corrected this line
 @pass_db
-def remote(db,action):
+def remote(db, action):
     """Enable or disable remote kdump feature"""
     kdump_table = db.cfgdb.get_table("KDUMP")
     check_kdump_table_existence(kdump_table)
