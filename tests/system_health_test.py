3e44844c312372abf28c47253bdb7441ec1e69d1
Ramesh Raghupathy <ram@cisco.com>
2024-09-29 16:30:44 -0700

    Debugging

--- tests/system_health_test.py
+++ tests/system_health_test.py
@@ -63,13 +63,15 @@ class MockerChassis(object):
 import show.main as show
 
 class TestHealth(object):
+    original_cli = None
 
     @classmethod
     def setup_class(cls):
         print("SETUP")
         os.environ["PATH"] += os.pathsep + scripts_path
         os.environ["UTILITIES_UNIT_TESTING"] = "1"
-        global original_cli = show.cli
+        global original_cli
+        original_cli = show.cli
 
     def test_health_summary(self):
         runner = CliRunner()
