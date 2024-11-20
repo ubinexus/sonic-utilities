1b0676806b59dd51e47ae6181cf9cdcc2e767011
kanza.latif <kanzalateef@gmail.com>
2024-11-20 17:42:06 +0500

    updated test cases

--- tests/show_memory_statistics_test.py
+++ tests/show_memory_statistics_test.py
@@ -1,5 +1,5 @@
 import pytest
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 from click.testing import CliRunner
 import json
 from show.memory_statistics import cli, Dict2Obj, send_data
