88af21da0ac7a48a21d293efa0a79994527e53c9
Vasundhara Volam <vvolam@microsoft.com>
2024-11-28 04:55:17 +0000

    Define a new try_get_args() which takes arguments as inputs

--- tests/test_module.py
+++ tests/test_module.py
@@ -13,17 +13,22 @@ module_helper = ModuleHelper()
 class TestModuleHelper:
     @pytest.fixture
     def mock_load_platform_chassis(self):
-        with mock.patch('utilities_common.module_base.util.load_platform_chassis') as mock_load_platform_chassis:
+        with mock.patch('utilities_common.module.util.load_platform_chassis') as mock_load_platform_chassis:
             yield mock_load_platform_chassis
 
     @pytest.fixture
     def mock_try_get(self):
-        with mock.patch('utilities_common.module_base.util.try_get') as mock_try_get:
+        with mock.patch('utilities_common.module.util.try_get') as mock_try_get:
             yield mock_try_get
 
     @pytest.fixture
+    def mock_try_get_args(self):
+        with mock.patch.object(ModuleHelper, 'try_get_args') as mock_try_get_args:
+            yield mock_try_get_args
+
+    @pytest.fixture
     def mock_log_error(self):
-        with mock.patch('utilities_common.module_base.log.log_error') as mock_log_error:
+        with mock.patch('utilities_common.module.log.log_error') as mock_log_error:
             yield mock_log_error
 
     def test_init_success(self, mock_load_platform_chassis):
@@ -37,22 +42,23 @@ class TestModuleHelper:
         mock_log_error.assert_called_once_with("Failed to load platform chassis")
         assert module_helper.platform_chassis is None
 
-    def test_reboot_module_success(self, mock_load_platform_chassis, mock_try_get):
-        mock_try_get.return_value = True
+    def test_reboot_module_success(self, mock_load_platform_chassis, mock_try_get_args):
+        mock_try_get_args.return_value = True
         mock_load_platform_chassis.return_value.get_module_index.return_value = 1
         mock_load_platform_chassis.return_value.get_module.return_value.reboot.return_value = True
 
         result = module_helper.reboot_module("DPU1", "cold")
         assert result is True
 
-    def test_reboot_module_invalid_index(self, mock_load_platform_chassis, mock_try_get):
-        mock_try_get.return_value = INVALID_MODULE_INDEX
+    def test_reboot_module_invalid_index(self, mock_load_platform_chassis, mock_try_get_args):
+        mock_try_get_args.return_value = INVALID_MODULE_INDEX
         mock_load_platform_chassis.return_value.get_module_index.return_value = INVALID_MODULE_INDEX
 
         result = module_helper.reboot_module("DPU1", "cold")
         assert result is False
 
-    def test_pci_detach_module_success(self, mock_load_platform_chassis, mock_try_get):
+    def test_pci_detach_module_success(self, mock_load_platform_chassis, mock_try_get_args, mock_try_get):
+        mock_try_get_args.return_value = True
         mock_try_get.return_value = True
         mock_load_platform_chassis.return_value.get_module_index.return_value = 1
         mock_load_platform_chassis.return_value.get_module.return_value.pci_detach.return_value = True
@@ -60,14 +66,15 @@ class TestModuleHelper:
         result = module_helper.pci_detach_module("DPU1")
         assert result is True
 
-    def test_pci_detach_module_invalid_index(self, mock_load_platform_chassis, mock_try_get):
-        mock_try_get.return_value = INVALID_MODULE_INDEX
+    def test_pci_detach_module_invalid_index(self, mock_load_platform_chassis, mock_try_get_args):
+        mock_try_get_args.return_value = INVALID_MODULE_INDEX
         mock_load_platform_chassis.return_value.get_module_index.return_value = INVALID_MODULE_INDEX
 
         result = module_helper.pci_detach_module("DPU1")
         assert result is False
 
-    def test_pci_reattach_module_success(self, mock_load_platform_chassis, mock_try_get):
+    def test_pci_reattach_module_success(self, mock_load_platform_chassis, mock_try_get_args, mock_try_get):
+        mock_try_get_args.return_value = True
         mock_try_get.return_value = True
         mock_load_platform_chassis.return_value.get_module_index.return_value = 1
         mock_load_platform_chassis.return_value.get_module.return_value.pci_reattach.return_value = True
@@ -75,8 +82,8 @@ class TestModuleHelper:
         result = module_helper.pci_reattach_module("DPU1")
         assert result is True
 
-    def test_pci_reattach_module_invalid_index(self, mock_load_platform_chassis, mock_try_get):
-        mock_try_get.return_value = INVALID_MODULE_INDEX
+    def test_pci_reattach_module_invalid_index(self, mock_load_platform_chassis, mock_try_get_args):
+        mock_try_get_args.return_value = INVALID_MODULE_INDEX
         mock_load_platform_chassis.return_value.get_module_index.return_value = INVALID_MODULE_INDEX
 
         result = module_helper.pci_reattach_module("DPU1")
--- utilities_common/module.py
+++ utilities_common/module.py
@@ -25,6 +25,24 @@ class ModuleHelper:
         if not self.platform_chassis:
             log.log_error("Failed to load platform chassis")
 
+    def try_get_args(self, callback, *args, **kwargs):
+        """
+        Handy function to invoke the callback and catch NotImplementedError
+        :param callback: Callback to be invoked
+        :param args: Arguments to be passed to callback
+        :param kwargs: Default return value if exception occur
+        :return: Default return value if exception occur else return value of the callback
+        """
+        default = kwargs.get('default', NOT_AVAILABLE)
+        try:
+            ret = callback(*args)
+            if ret is None:
+                ret = default
+        except NotImplementedError:
+            ret = default
+
+        return ret
+
     def reboot_module(self, module_name, reboot_type):
         """
         Reboot the specified module by invoking the platform API.
@@ -36,7 +54,7 @@ class ModuleHelper:
         Returns:
             bool: True if the reboot command was successfully sent, False otherwise.
         """
-        module_index = util.try_get(self.platform_chassis.get_module_index, module_name, default=INVALID_MODULE_INDEX)
+        module_index = self.try_get_args(self.platform_chassis.get_module_index, module_name, default=INVALID_MODULE_INDEX)
         if module_index < 0:
             log.log_error("Unable to get module-index for {}". format(module_name))
             return False
@@ -46,7 +64,7 @@ class ModuleHelper:
             return False
 
         log.log_info("Rebooting module {} with reboot_type {}...".format(module_name, reboot_type))
-        status = util.try_get(self.platform_chassis.get_module(module_index).reboot, reboot_type, False)
+        status = self.try_get_args(self.platform_chassis.get_module(module_index).reboot, reboot_type, False)
         if not status:
             log.log_error("Reboot status for module {}: {}".format(module_name, status))
             return False
@@ -63,7 +81,7 @@ class ModuleHelper:
         Returns:
             bool: True if the detach command was successfully sent, False otherwise.
         """
-        module_index = util.try_get(self.platform_chassis.get_module_index, module_name, default=INVALID_MODULE_INDEX)
+        module_index = self.try_get_args(self.platform_chassis.get_module_index, module_name, default=INVALID_MODULE_INDEX)
         if module_index < 0:
             log.log_error("Unable to get module-index for {}". format(module_name))
             return False
@@ -90,7 +108,7 @@ class ModuleHelper:
         Returns:
             bool: True if the rescan command was successfully sent, False otherwise.
         """
-        module_index = util.try_get(self.platform_chassis.get_module_index, module_name, default=INVALID_MODULE_INDEX)
+        module_index = self.try_get_args(self.platform_chassis.get_module_index, module_name, default=INVALID_MODULE_INDEX)
         if module_index < 0:
             log.log_error("Unable to get module-index for {}". format(module_name))
             return False
